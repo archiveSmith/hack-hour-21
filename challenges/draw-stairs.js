be582acef8a3f0063a6116075ca4c2088ad3e7bc
Kyle <kaloftin@hotmail.com>
2018-04-20 10:03:13 -0700

    Draw-Stairs Done

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,7 +14,12 @@
 */
 
 function drawStairs(n) {
-
+  const line = Array(n + 1).fill(' ');
+  line[n] = '';
+  for (let i = n - 1; i >= 0; i--) {
+    line[i] = '*';
+    console.log(line.join(''));
+  }
 }
 
 
