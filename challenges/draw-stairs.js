e8e0cbbf6947d1085edc572d579c640166705c5e
Jessica Skok <jessicaskok@gmail.com>
2018-04-20 10:51:30 -0700

    working on draw stairs

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -13,8 +13,24 @@
 
 */
 
-function drawStairs(n) {
+// if n is greater than 100 or less than 1, print error message
+// need to... get n, start at 1, next line. 2, next line. 3, etc... until the pyramid is created.
+//actually - need to get n and put x amount of white space in front of each asterisk
+//white space is related inversely to number of asterisks
+//top white space [5] is equal to the number of asterisks at the bottom minus 1
+//top white space is n-1... second is n-2, etc. until n=n!
 
+function drawStairs(n) {
+    var asterisk = '*';
+    var space = '';
+    if (n > 100 || n < 1){
+        console.log('Error! Your number is bigger than 100 or smaller than 1');
+    }
+    else {
+        for (var i = 0; i < n; i++){
+            console.log()
+        }
+    }
 }
 
 
