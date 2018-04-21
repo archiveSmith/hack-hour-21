15a358d56d3a6a35e6d3b188fe4a554630a0ea0f
Kyle <kaloftin@hotmail.com>
2018-04-21 10:05:12 -0700

    Fineshed Mode/Mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,28 @@
 
 
 function modemean(array) {
-
+  let mode = {};
+  let mean = 0;
+  let map = {};
+  let count = 0;
+  
+  array.forEach(function findMode(num) {
+    map[num] = (map[num] || 0) + 1;
+    if (count < map[num]) {
+      count = map[num];
+      mode = num;
+    }
+    mean += num;
+    if(mode[num] === undefined){
+      mode[num] = 0;
+    }        
+      mode[num] += 1;
+  });
+  mean = mean / array.length;
+  if (Math.floor(mode) === mean) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = modemean;
