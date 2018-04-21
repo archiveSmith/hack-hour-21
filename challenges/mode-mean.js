5a3c54d7ee4302e68ba5640f3df8d2d116f37023
Christopher <chris132128@gmail.com>
2018-04-21 09:42:41 -0700

    mean-mode complete

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,25 @@
 
 
 function modemean(array) {
-
+    let cache = {};
+    let mean = 0;
+    let mode = 0;
+    array.forEach(ele => {
+        if (cache[ele]) cache[ele] += 1;
+        else cache[ele] = 1;
+        mean += ele;
+    });
+    let largest = cache[array[0]];
+    mean = Math.floor(mean / array.length);
+    for (let key in cache) {
+        if (cache[key] > largest) {
+        largest = cache[key];
+        mode = key;
+        } if (cache[key] == largest) {
+          if (key > mode) mode = key;
+        }
+    }
+    return mode == mean;
 }
 
 module.exports = modemean;
