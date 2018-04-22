51b6974bb9e5429a9d9ba537054d0f532cefcd9a
Stacy Adelsberger <sladelsberger@gmail.com>
2018-04-21 17:29:25 -0700

    mode-mean appears to work

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,42 @@
  *
  */
 
-
 function modemean(array) {
-
+    let mean;
+    let mode;
+    var modeMap = {};
+    
+    var sum = array.reduce(function (acc, curr){
+        return acc + curr;
+    }, 0);
+    
+    mean = Math.floor(sum / array.length);
+    
+    var most = array.reduce(function(acc, curr) {
+        if (curr in acc) {
+            acc[curr]++;
+        } else {
+            acc[curr] = 1
+        }
+        return acc;
+    }, {});
+    
+    
+    var mostKeysArr = Object.keys(most);
+    mode = mostKeysArr.reduce(function(acc, curr) {
+        acc = most[acc] > most[curr] ? Number(acc) : Number(curr);
+        return acc;
+    })
+    
+    
+    
+    if(mode === mean) {
+        return true;
+    } else {
+        return false;
+    }
+    
+    
 }
 
 module.exports = modemean;
