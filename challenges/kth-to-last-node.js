a6757bb43ea6622f8e811e6cbf59a98c4e284b67
Stefan <stefan.armijo@gmail.com>
2018-04-24 09:52:01 -0700

    implement kth-to-last-node

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -16,13 +16,66 @@
  * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
  */
 
+function List() {
+    this.head = null;
+}
+
+List.prototype.add = function(val) {
+    if (this.head === null) {
+        this.head = new Node(val);
+    } else {
+        let currNode = this.head;
+        
+        while (currNode.next !== null) {
+            currNode = currNode.next;
+        }
+
+        currNode.next = new Node(val);
+    }
+}
+
+    
+
 function Node(val) {
   this.value = val;
   this.next = null;
 }
 
+
 function kthToLastNode(k, head) {
 
+    if (head === undefined) {
+        return undefined;
+    }
+
+    const valArr = [head.value];
+
+    while (head.next !== null) {
+        head = head.next;
+        valArr.push(head.value);
+    }
+
+    let i = 1;
+    for (let j = valArr.length -1; j >= 0; j -= 1) {
+        if (i === k) {
+            return valArr[j];
+        }
+        i += 1;
+    }
+
+    return undefined;
 }
+
+
+// let l = new List();
+// l.add('A');
+// l.add('B');
+// l.add('C');
+// l.add('D');
+// l.add('E');
+// 
+// let a = l.head;
+// 
+// console.log(kthToLastNode(5, a));
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
