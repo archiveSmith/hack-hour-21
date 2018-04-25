6c048c2c2e7087910ff669420e9a996b76d2a31b
Colin McCarthy <colin@Colins-MacBook-Pro.local>
2018-04-25 09:45:19 -0700

    refactor for negative exponents

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,24 @@
  */
 
 function pow(base, power) {
-
+  if (!base && !power) return;
+  function recurPow(base, power) {
+    if (power === 0) return 1;
+    return base * pow(base, Math.abs(power) - 1);
+  }
+  const returnVal = recurPow(base, power);
+  if (power < 0) return 1 / returnVal;
+  return returnVal;
 }
+
+console.log(pow(0, 0)); // --> undefined
+
+console.log(4, 0); // --> 1
+
+console.log(4, 4); // --> 256
+
+console.log(pow(2, -4)); // --> 1/16th
+
+console.log(1 / 16);
 
 module.exports = pow;
