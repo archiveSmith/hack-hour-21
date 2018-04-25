46988adfe61cdad772fbc920e05cd10fc211a2c6
William LeGate <william@legate.me>
2018-04-25 09:09:26 -0700

    Completes 'pow' challege

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,21 @@
  * Use recursion!
  */
 
-function pow(base, power) {
+function pow(base, power, prevVal = 1) {
+    if (power > 0) {
+        prevVal *= base;
+        power -= 1;
+    }
+    else if (power < 0) {
+        prevVal /= base;
+        power += 1;
+    }
 
+    return power === 0 ? prevVal : pow(base, power, prevVal);
 }
+
+// console.log(pow(5, 3)); // expect 125
+// console.log(pow(2, 6)); // expect 64
+// console.log(pow(5, -2)); // expect 1/25
 
 module.exports = pow;
