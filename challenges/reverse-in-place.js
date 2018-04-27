1ad35877b80017ac726c5e9dbad8237671314eeb
Jeff <jeffreyrkang@gmail.com>
2018-04-27 09:35:41 -0700

    Complete reverseinplace

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,7 +14,12 @@
  */
 
 function reverseInPlace(array) {
-
+  for (let i = 0; i < Math.floor(array.length/2); i++) {
+    let temp = array[i];
+    array[i] = array[array.length - i - 1];
+    array[array.length - i - 1] = temp;
+  }
+  return array;
 }
 
 module.exports = reverseInPlace;
