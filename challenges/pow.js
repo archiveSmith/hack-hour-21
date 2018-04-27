bcf652386ab6d898845112b8e40156d343781caa
Christopher <chris132128@gmail.com>
2018-04-27 09:32:46 -0700

    pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,19 +3,22 @@
  */
 
 function pow(base, power) {
-    if (power < 0) power = Math.abs(power);
-    const negative = true;
-    const originalBase = base;
-    const newFunc = (acc, power2) => {
-      if (power2 < 0) power2 += 1;
-      if (power2 === 0) return 1;
-      if (power2 === 1) return acc;
-      acc *= originalBase;
-      power2 -= 1;
-      return newFunc(acc, power2);
-    }
-    if (negative) return 1 / newFunc(base, power);
-    return newFunc(base, power);
+  let negative = false;
+  if (power < 0) {
+    power = Math.abs(power);
+    negative = true;
   }
+  const originalBase = base;
+  const newFunc = (acc, power2) => {
+    if (power2 < 0) power2 += 1;
+    if (power2 === 0) return 1;
+    if (power2 === 1) return acc;
+    acc *= originalBase;
+    power2 -= 1;
+    return newFunc(acc, power2);
+  };
+  if (negative) return 1 / newFunc(base, power);
+  return newFunc(base, power);
+}
 
 module.exports = pow;
