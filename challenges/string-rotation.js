a4498dc1bb218652a2d1fc46de92b7d0ee04995d
William Adamowicz <william.adamowicz@gmail.com>
2018-04-28 09:25:33 -0700

    complete string rotation

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,7 +16,8 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
-
+  s1 = s1 + s1;
+  return isSubstring(s1, s2);
 }
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
