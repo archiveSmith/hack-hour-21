8003dc8469432605399339b5e655e6867da2ca48
Colin McCarthy <colin@Colins-MacBook-Pro.local>
2018-05-01 09:59:48 -0700

    finish balanced parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,45 @@
  *
  */
 
-function balancedParens(input){
-
+function balancedParens(input) {
+  // function to return a valid opening bracket for a given closing bracket
+  const valid = (char) => {
+    if (char === '}') return '{';
+    if (char === ']') return '[';
+    if (char === ')') return '(';
+    return null;
+  };
+  // counters
+  let numOpen = 0;
+  let numClosed = 0;
+  // regex of all pertinent brackets
+  const brackets = /\}|\)|\]|\{|\[|\(/;
+  // array to track the last unclosed bracket
+  const savedLastEs = [];
+  // split array and reduce to return a boolean
+  return input.split('').reduce((acc, char) => {
+    // do work only if boolean is still true
+    if (acc) {
+      // skip current char if not a bracket
+      if (!brackets.test(char)) return acc;
+      // if opening bracket...
+      if (/\{|\(|\[/.test(char)) {
+        numOpen += 1;
+        // add opening bracket to end of saved brackets array
+        savedLastEs.push(char);
+      }
+      // if closing bracket...
+      if (/\}|\)|\]/.test(char)) {
+        numClosed += 1;
+        // main test - check to ensure a closing bracket does not appear
+        // after an opening bracket of a different type
+        if (savedLastEs[savedLastEs.length - 1] !== valid(char)) return false;
+        // remove properly closed closing bracket from end of saved brackets array
+        savedLastEs.pop();
+      }
+    }
+    return acc;
+  }, true) && (numOpen === numClosed); // returning reduce boolean + counters equivalency
 }
 
 module.exports = balancedParens;
