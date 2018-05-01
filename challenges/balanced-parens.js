11828b94004d50100c8f6bbc3f0b6d64de6b46d8
Jeff <jeffreyrkang@gmail.com>
2018-05-01 09:36:55 -0700

    Complete balanced-parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,41 @@
  *
  */
 
-function balancedParens(input){
-
+function balancedParens(input) {
+  let brackets = 0;
+  let parens = 0;
+  let braces = 0;
+  for (let i = 0; i < input.length; i += 1) {
+    if (input.charAt(i) === '[') {
+      brackets += 1;
+      for (let j = i + 1; j < input.length; j += 1) {
+        if (input.charAt(j) === ']') {
+          brackets += 1;
+          input = input.slice(0, j) + input.slice(j + 1, input.length);
+          break;
+        }
+      }
+    } else if (input.charAt(i) === '(') {
+      parens += 1;
+      for (let j = i + 1; j < input.length; j += 1) {
+        if (input.charAt(j) === ')') {
+          parens += 1;
+          input = input.slice(0, j) + input.slice(j + 1, input.length);
+          break;
+        }
+      }
+    } else if (input.charAt(i) === '{') {
+      braces += 1;
+      for (let j = i + 1; j < input.length; j += 1) {
+        if (input.charAt(j) === '}') {
+          braces += 1;
+          input = input.slice(0, j) + input.slice(j + 1, input.length);
+          break;
+        }
+      }
+    } else if (input.charAt(i) === ']' || input.charAt(i) === ')' || input.charAt(i) === '}') return false;
+  }
+  return brackets % 2 === 0 && parens % 2 === 0 && braces % 2 === 0;
 }
 
 module.exports = balancedParens;
