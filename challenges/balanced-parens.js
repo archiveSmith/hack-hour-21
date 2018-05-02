f5dd83e686832928d0c0883681d9407d46c42487
Stacy Adelsberger <sladelsberger@gmail.com>
2018-05-01 21:55:22 -0700

    incomplete

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,27 @@
  */
 
 function balancedParens(input){
-
+    let isBalanced = [];
+    let openBracket = "[(\]{]";
+    let closeBracket = "[)\]}]";
+    console.log(openBracket);
+    
+    if (input[0] === '[' && input[1] === ']') {
+      return true;
+    } else {
+      return false;
+    }
+    
+    for (var i = 0; i < input.length; i++) {
+      // simple test: if input[0] is equal to input[input.length - 1], return true
+      // complex test: opening "bracket" must match a closing "bracket" with nothing getting in way. 
+        // if input[i] is an opening "bracket" push it onto stack
+        // if input[i] is a closing "bracket" check if it matches the most recent opening bracket. 
+          // if so pop both items from stack and repeat
+          // if the stack empties and is equal to the string being finished, then return true
+          // if at any time, a closing bracket does NOT match a most recent opening bracket return false.
+    }
+    
 }
 
 module.exports = balancedParens;
