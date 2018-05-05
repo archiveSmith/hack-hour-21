e20ffab42d50c3835261b40dcca7b302e1c164d0
Jessica Skok <jessicaskok@gmail.com>
2018-05-05 09:58:34 -0700

    subset sum

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -6,9 +6,19 @@
  * subsetSum([3, 34, 4, 12, 5, 12], 32) -> true, 3 + 12 + 5 + 12 = 32
  * subsetSum([8, 2, 4, 12], 13) -> false
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
+ * 
+ * //loop through array first checking to see if one number = the target.
+ * if not, then loop through with two, three, etc.
  */
 
 function subsetSum(array, target) {
+    for (let i = 0; i < array.length; i++){
+        if (array[i] === target) return true;
+    for (let k = i + 1; k < array.length; k++){
+      if (array[i] + array[k] === target) return true;
+     }
+    }
+    return false;
 
 }
 
