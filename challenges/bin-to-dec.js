aacc3dd8537eb87e5d6cd35f424fbdc7f1194ac6
Will Howard <Will@Wills-MacBook-Pro.local>
2018-05-05 10:25:39 -0700

    hack soln 5/5

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,27 @@
  */
 
 function binToDec(binary) {
+    let binInput = binary.split('');
+    let inBinArr = [];
+    for (let i = 0; i < binInput.length; i++) {
+        let temp = parseInt(binInput[i]);
+        inBinArr[i] = temp;
+    }
+    inBinArr.reverse();
 
+    let numArr = [1, 2, 4, 8, 16, 32, 64, 128, 256];
+
+    let dec = 0;
+    for (let i = inBinArr.length - 1; i >= 0; i--) {
+        //console.log(inNumArr[i]);
+        if (inBinArr[i] === 1) {
+            dec += numArr[i];
+        }
+    }
+    return dec;
 }
+
+console.log(binToDec('0101'));
+console.log(binToDec('100'));
 
 module.exports = binToDec;
