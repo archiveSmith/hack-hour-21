4ee5108277dcc9437a349df7dac63f8a6550bee0
Mystique <helenk530@yahoo.com>
2018-05-05 09:28:43 -0700

    subset sum

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -6,6 +6,11 @@
  * subsetSum([3, 34, 4, 12, 5, 12], 32) -> true, 3 + 12 + 5 + 12 = 32
  * subsetSum([8, 2, 4, 12], 13) -> false
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
+ 
+ input: {array}/numbers and {number}/target
+ output: {true} if a subset of the array sums up to the target.
+  false otherwise.
+
  */
 
 function subsetSum(array, target) {
