28c8af7656500110d254f7bec2874883fb2db508
Stacy Adelsberger <sladelsberger@gmail.com>
2018-05-05 10:02:06 -0700

    binary successfully converts to decimal

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,32 @@
  */
 
 function binToDec(binary) {
-
-}
+    let length = binary.length;
+    let pow = length - 1;
+    // console.log("length", length);
+    let arr = [];
+    let obj = {};
+    for (let i = 0; i < length; i++) {
+      arr.push(Math.pow(2, pow));
+      pow --;
+    }
+    
+    for (let j = 0; j < arr.length; j++) {
+      obj[arr[j]] = binary[j];
+    }
+    var dec = 0;
+    for (var key in obj) {
+      if (obj[key] === '1') {
+        dec += Number(key);
+      }
+      // console.log(obj[key], "working?");
+      // console.log (key, "...");
+      // console.log("YAY?", dec);
+    }
+    
+    // console.log("obj", obj);
+    // console.log("arr", arr);
+    return dec;
+  }
 
 module.exports = binToDec;
