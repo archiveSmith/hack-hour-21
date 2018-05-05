a23ad3943161c9250b435cf2b72a55027e19276a
William LeGate <william@legate.me>
2018-05-05 09:50:54 -0700

    Completes bin-to-dec.js challenge

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -13,8 +13,19 @@
  * Write a function that converts a decimal number to binary (then maybe hexadecimal)
  */
 
-function binToDec(binary) {
-
+function binToDec(binary, index = 0, sum = 0) {
+    if (binary.length === 0) return sum;
+    const lastVal = Number(binary.substring(binary.length - 1, binary.length));
+    if (lastVal === 1) sum += Math.pow(2, index);
+    index += 1
+    binary = binary.slice(0 , -1); // remove last element from string
+    return binToDec(binary, index, sum);
 }
+
+console.log(binToDec('0'));     //  -> 0
+console.log(binToDec('11'));    // -> 3
+console.log(binToDec('100'));   // -> 4
+console.log(binToDec('101'));   // -> 5
+console.log(binToDec('0101'));  // -> 5
 
 module.exports = binToDec;
