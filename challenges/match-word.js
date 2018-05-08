58102015b2e81e39ea9544f4744a8c4078207c59
Jessica Skok <jessicaskok@gmail.com>
2018-05-08 10:21:54 -0700

    word match didnt finish

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -9,9 +9,49 @@
 // matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ] 
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
+//shift all to caps
+//loop to middle
+//must have space in the middle of words
+
+//shift all to caps
+//loop to middle
+//must have space in the middle of words
 
 function matchWord(str) {
-
-}
-
+  let capsWord = str.toUpperCase();
+  capsWord = capsWord.replace(/\W/g, '');
+  console.log(capsWord);
+  let strArray = [];
+  let backwardsStrArray = [];
+  
+  if (capsWord.includes('_')){
+  for (var i = 0; i < capsWord.length; i++){
+      strArray.push(capsWord[i]);
+  }
+  for (var z = capsWord.length - 1; z > 0; z--){
+      backwardsStrArray.push(capsWord[z]);
+  }
+  
+  return arraysEqual(strArray, backwardsStrArray);
+  }
+  
+  else {
+    return false;
+  }
+  }
+  
+  function arraysEqual(arr1, arr2) {
+      if(arr1.length !== arr2.length)
+          return false;
+      for(var i = arr1.length; i--;) {
+          if(arr1[i] !== arr2[i])
+              return false;
+      }
+  
+      return true;
+  }
+  
+   matchWord('__END_DNE-----');
+   
 module.exports = matchWord;
+
