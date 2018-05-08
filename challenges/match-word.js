15ed5bce715eb97757d67fc6b00cf9a1154a196c
William LeGate <william@legate.me>
2018-05-08 09:46:44 -0700

    Completes match-word challenge

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -10,8 +10,57 @@
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
 
+
+/* 
+ * Regex expression… checks if all characters are letters, case insensitive
+ * (ex: IS_WORD.text(str))
+*/ 
+const IS_WORD = /^[a-zA-Z]+$/; 
+
+const reverse = str => Array.from(str).reverse().join('');
+
 function matchWord(str) {
 
+    const openWords = [];
+
+    let currentWord = "";
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str.charAt(i);
+        const isWord = IS_WORD.test(char);
+
+        if (isWord) {
+            // append char to currentWord & convert all to lowercase
+            currentWord += char.toLowerCase();
+        }
+        
+        if ((!isWord || (i + 1) === str.length) && currentWord.length > 0) {
+            // check if currentWord closes out previous word if at end of currentWord or end of str
+            if (reverse(currentWord) === openWords[openWords.length - 1]) {
+                // pop last openWord if the currentWord closes it out
+                openWords.pop();
+            } 
+            else {
+                // push currentWord to openWords if it doesn't close out the last open word
+                openWords.push(currentWord);
+            }
+
+            // reset the currentWord
+            currentWord = "";
+        }
+        else {
+            // ignore non-word characters if we don't have a currentWord
+        }
+    }
+
+    return openWords.length === 0;
 }
+
+// console.log(matchWord('__END_DNE-----'));                                   // -> true
+// console.log(matchWord('__ENDDNE__'))                                        // -> false       (not separated by a space)
+// console.log(matchWord('IF()()fi[]'));                                       // -> true        (should be case-insensitive)
+// console.log(matchWord('for__if__rof__fi'));                                 // -> false     not properly closed. like ( [) ] 
+// console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));  // -> true
+// console.log(matchWord(''));                                                 // -> true
 
 module.exports = matchWord;
