6bf2512fa7a0a42b7075b8a73f5a37010740fb71
Stefan <stefan.armijo@gmail.com>
2018-05-08 10:17:57 -0700

    match words

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -1,35 +1,27 @@
 /* You are given an array of integers and a target number. Write a function that returns true if
- * there is a subset of the array that sums up to the target and returns false otherwise. A subset
- * can be any size and the elements do not have to appear consecutively in the array.
- * 
- * subsetSum([3, 7, 4, 2], 5) - > true, 3 + 2 = 5
- * subsetSum([3, 34, 4, 12, 5, 12], 32) -> true, 3 + 12 + 5 + 12 = 32
- * subsetSum([8, 2, 4, 12], 13) -> false
- * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
- */
+* there is a subset of the array that sums up to the target and returns false otherwise. A subset
+* can be any size and the elements do not have to appear consecutively in the array.
+* 
+* subsetSum([3, 7, 4, 2], 5) - > true, 3 + 2 = 5
+* subsetSum([3, 34, 4, 12, 5, 12], 32) -> true, 3 + 12 + 5 + 12 = 32
+* subsetSum([8, 2, 4, 12], 13) -> false
+* subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
+*/
+
+
+
+
 
 function subsetSum(array, target) {
-
-    // begin generating all possible combinations of the array and see if they sum to target
-    
-    // check if there are any t - n and n elements. if there is a t - n but no n, remove t - n.
-   
-    // generate a combination of array and check if it sums to target
-    var fn = function(active, rest, a) {
-        if (!active && !rest)
-            return;
-        if (!rest) {
-            a.push(active);
-        } else {
-            fn(active + rest[0], rest.slice(1), a);
-            fn(active, rest.slice(1), a);
-        }
-        if (a.reduce((a, c) => a + c) === target) {
-            return true
-        }
-    }
-    return fn([], array, []);
-    
+  console.log(array, target);
+  if (!target) return true;
+  if (!array.length) return false;
+  return subsetSum(array.slice(1), target - array[0]) || subsetSum(array.slice(1), target);
 }
 
-module.exports = subsetSum;
+
+
+console.log(subsetSum([3,7,4,2], 5)); //true, 5
+// console.log(subsetSum([3, 34, 4, 12, 5, 12], 32)) //-> true, 3 + 12 + 5 + 12 = 32
+// console.log(subsetSum([8, 2, 4, 12], 13)) //-> false
+// console.log(subsetSum([8, -2, 1, -3], 6)) //-> true, 8 + 1 + (-3) = 6
