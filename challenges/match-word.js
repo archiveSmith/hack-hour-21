254a93241a20446349b8cd13f114110c37d52799
James Tu <tu.james@gmail.com>
2018-05-08 09:41:34 -0700

    match word

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -11,7 +11,51 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+    let s = str;
+    if (s === '') return true;
+
+    // convert everything to lowercase
+    s = s.toLowerCase();
+
+    // split the string by non-characters
+    let wordArray = s.split(/[^a-z]+/);
+    // console.log(wordArray);
+
+    let stack = [];
+    let currWord = '';
+    // now do our stack method for matching pairs
+    for(let i=0; i<wordArray.length; i++){
+        currWord = wordArray[i];
+        if (currWord === '') continue;
+        
+        // check against the reverse of the work on the top of the stack
+        if ((stack.length === 0) || (stack[stack.length - 1] !== reverseString(currWord)) ) {
+            stack.push(currWord);
+        } else if ((stack[stack.length - 1] === reverseString(currWord)) ) {
+            // we have a match, pop the top of the stack
+            let t=stack.pop();
+            // console.log(t);
+        }
+
+    }
+
+    if (stack.length !== 0) return false;
+    return true;
 
 }
+
+// console.log(matchWord("%%$@$while  try ! yrt  for if_fi rof #*#  elihw"));
+// console.log(matchWord("__ENDDNE__"));
+// console.log(matchWord("for__if__rof__fi"));
+// console.log(matchWord("IF()()fi[]"));
+
+
+
+// helper function to reverse string
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+// console.log(reverseString('hello'));
 
 module.exports = matchWord;
