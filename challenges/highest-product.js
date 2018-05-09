131423682e3475dd3d84fb6e1ab0c587e88ff6c8
Arman Bedrosyan <redarman@hotmail.com>
2018-05-09 09:24:20 -0700

    commit 5/9 hackhour

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -1,9 +1,13 @@
 /**
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
+// highestProduct([1,4,5,67,8,9,6,43,5,3,8])
+
 
 function highestProduct(array) {
-
+    if(array.length < 3) return 'array not long enough';
+    const arr = array.sort((a,b) => b > a)
+    return arr[0] * arr[1] * arr[2]
 }
 
 
