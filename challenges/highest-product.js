178b167524e272de6de009a436d6fbb5dc7b4cf2
Jessica Skok <jessicaskok@gmail.com>
2018-05-09 09:44:28 -0700

    highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,6 +3,17 @@
  */
 
 function highestProduct(array) {
+  let sum = 0;
+  let tempNum = 0;
+  for (let i = 0; i < array.length; i++){
+    for (let z = 0; z < array.length; z++){
+      for (let x = 0; x < array.length; x++){
+        tempNum = array[i] * array[x] * array[z];
+        if (tempNum > sum) sum = tempNum;
+      }
+    }
+  }
+  return sum;
 
 }
 
