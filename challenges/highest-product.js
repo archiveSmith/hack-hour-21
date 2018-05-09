9daad161e828c2c538df6919aa223b8509588966
Christopher <chris132128@gmail.com>
2018-05-09 09:26:00 -0700

    hack hour 5/9

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,16 @@
  */
 
 function highestProduct(array) {
-
+  let biggest = [];
+  for (let i = 0; i < 3; i += 1) {
+    array.forEach((ele, index) => {
+      if (index === 0) biggest[i] = ele;
+      if (ele > biggest[i]) biggest[i] = ele;
+    });
+    array.splice(array.indexOf(biggest[i]), 1);
+    console.log(array);
+  }
+  return biggest[0] * biggest[1] * biggest[2];
 }
-
 
 module.exports = highestProduct;
