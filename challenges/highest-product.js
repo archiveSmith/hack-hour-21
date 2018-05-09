ac02bb81679b1d534caff627ae3122d0953fc815
Stacy Adelsberger <sladelsberger@gmail.com>
2018-05-09 09:39:23 -0700

    the highest product of 3 integers in array has been retrieved

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,22 @@
  */
 
 function highestProduct(array) {
-
-}
+    let total = 0
+    if (array.length < 3){
+      return false;
+    }
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+        for (let k = j + 1; k < array.length; k++) {
+          let product = array[i] * array[j] * array[k];
+          if (product > total) {
+            total = product;
+          }
+        }
+      }
+    }
+    return total;
+  }
 
 
 module.exports = highestProduct;
