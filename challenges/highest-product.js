d2be81154f72bdc214cb3b7fa6b2db07532de460
William LeGate <william@legate.me>
2018-05-09 10:41:32 -0700

    Completes highest-product challenge

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -4,7 +4,22 @@
 
 function highestProduct(array) {
 
-}
+    const highestNums = [];
+    const mutableArr = array.slice();
 
+    for (let i = 0; i < 3; i++) {
+        const max = Math.max(...mutableArr);
+        const index = array.indexOf(max);
+        if (index > -1) {
+            mutableArr.splice(index, 1);
+        }
+    }
+
+    const product = mutableArr.reduce((acc, cur) => {
+        acc * cur;
+    }, 1);
+    
+    return product;
+}
 
 module.exports = highestProduct;
