f2d18342d0836204b2b4ea73ea9202fe4b805146
Kyle <kaloftin@hotmail.com>
2018-05-09 09:41:56 -0700

    Highest Prod Done

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,9 @@
  */
 
 function highestProduct(array) {
-
+  const arr = array.sort((a, b) => a - b);
+  const sum = arr[arr.length - 3] * arr[arr.length - 2] * arr[arr.length - 1];
+  return sum;
 }
 
 
