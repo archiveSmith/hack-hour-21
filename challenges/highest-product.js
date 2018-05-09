535ec9138c90b618dfda5b2627b99b48e1cb061f
Jeff <jeffreyrkang@gmail.com>
2018-05-09 09:39:49 -0700

    Refactor highest-product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,19 +3,12 @@
  */
 
 function highestProduct(array) {
-  if (array.length === 0) return 0;
-  else if (array.length === 1) return array[0];
-  else if (array.length === 2) return array[0] * array[1];
-  else if (array.length === 3) return array[0] * array[1] * array[2];
-  let max = array[0];
-  for (let i = 0; i < array.length - 2; i += 1) {
-    for (let j = i + 1; j < array.length - 1; j += 1) {
-      for (let k = j + 1; k < array.length; k += 1) {
-        if (max < array[i] * array[j] * array[k]) max = array[i] * array[j] * array[k];
-      }
-    }
-  }
-  return max;
+  if (array.length < 3) return 0;
+  if (!Array.isArray(array)) return 0;
+  array.sort((a, b) => {
+    return b - a;
+  });
+  return array[0] * array[1] * array[2];
 }
 
 module.exports = highestProduct;
