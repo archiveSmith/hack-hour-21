88e04fd5b73d62ec6edde7ecac3cc24a146512e7
Christopher <chris132128@gmail.com>
2018-05-11 09:39:27 -0700

    zip-linked-list

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,33 +3,25 @@
  */
 
 function highestProduct(array) {
-  if (array.length < 3 || !Array.isArray(array)) return 0;
-  let biggest = [];
-  let counter = 0;
-  array.forEach(ele => {
-    if (ele > 0) counter += 1;
-  });
-  for (let i = 0; i < 3; i += 1) {
-    array.forEach((ele, index) => {
-      if (index === 0) biggest[i] = ele;
-      if (counter === 1) {
-        if (ele > biggest[i]) biggest[i] = ele;
-      }
-      if (counter < 1) {
-        if (ele < biggest[i]) biggest[i] = ele;
-      }
-      if (counter > 2) {
-        if (ele > biggest[i]) biggest[i] = ele;
-      }
-      if (counter === 2) {
-        if (ele > biggest[i]) biggest[i] = ele;
-      }
+    if (array.length < 3 || !Array.isArray(array)) return 0;
+    let biggest = [];
+    let counter = 0;
+    array.forEach(ele => {
+      if (ele < 0) counter += 1;
     });
-    if (counter === 1 || counter === 2) counter = 0;
-    array.splice(array.indexOf(biggest[i]), 1);
+    for (let i = 0; i < 3; i += 1) {
+      array.forEach((ele, index) => {
+        if (index === 0) biggest[i] = ele;
+        if (counter >= 1 && ele > biggest[i]) biggest[i] = ele;
+        else if (counter < 1 && ele < biggest[i]) biggest[i] = ele;
+      });
+      if (counter === 1 || counter === 2) counter = 0;
+      array.splice(array.indexOf(biggest[i]), 1);
+    }
+    console.log(biggest)
+    return biggest.reduce((acc, next) => acc * next);
   }
-  console.log(biggest);
-  return biggest.reduce((acc, next) => acc * next);
-}
+
+console.log(highestProduct([1, 2, -6, -7, -8, -9, -20]));
 
 module.exports = highestProduct;
--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,22 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  // connect .next to the next node in opposite list
+  // connect .next of squeezed in node to be old .next of original node
+  let temp;
+  let temp2;
+  let nextNode1 = l1;
+  let nextNode2 = l2;
+  
+  while (nextNode1 && nextNode2) {
+    temp = nextNode1.next;
+    nextNode1.next = nextNode2;
+    temp2 = nextNode2.next;
+    nextNode2.next = temp;
+
+    nextNode1 = temp;
+    nextNode2 = temp2;
+  }
 };
 
 module.exports = {Node: Node, zip: zip};
