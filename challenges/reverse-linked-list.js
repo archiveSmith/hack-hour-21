ce5e4775909cc17f1fbdf10301649791aa9edcf8
Jeff <jeffreyrkang@gmail.com>
2018-05-15 09:29:30 -0700

    Complete reverse-linked-list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,39 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-
+    if (!head) return undefined;
+    else if (head.next === null) return head;
+    let reverser = head;
+    while(reverser.next !== null) {
+        reverser = reverser.next;
+    }
+    let place = head;
+    let newHead = reverser;
+    while (reverser !== head) {
+        if (place.next === reverser) {
+            reverser.next = place;
+            reverser = reverser.next;
+            place = head;
+        }
+        else {
+            place = place.next;
+        }
+    }
+    reverser.next = null;
+    return newHead;
 }
+
+// let node1 = new Node(1);
+// node1.next = new Node(2);
+// node1.next.next = new Node(3);
+// node1.next.next.next = new Node(4);
+
+// const reversed = reverseLinkedList(node1);
+// console.log(reversed.value);
+// console.log(reversed.next.value);
+// console.log(reversed.next.next.value);
+// console.log(reversed.next.next.next.value);
+
+
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
