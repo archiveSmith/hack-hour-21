5fb829dec3ce4c9e2d91a7f0b117d461e6a26328
James <james@2-bit-toys.com>
2018-05-15 09:54:22 -0700

    reverse linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -15,6 +15,38 @@ function Node(value) {
 
 function reverseLinkedList(head) {
 
+  // if head is undefined or null
+  if ( !head ) return undefined;
+
+  // if the linked list only has one node
+  if ( !head.next ) return head; 
+
+
+  // Handle the head as a special case
+  let currentNode = head;
+  let nextNode = currentNode.next; // save this
+  let nextNextNode;
+
+  head.next = null; // b/c the head is now the tail and won't point to anything
+
+  while ( nextNode ) {
+    nextNextNode = nextNode.next; // save it
+    nextNode.next = currentNode; // reassign the pointer
+
+    // set this up for the next iteration
+    currentNode = nextNode;
+    nextNode = nextNextNode;
+  }
+
+  // need to return the new head
+  return currentNode;
+
 }
+
+// let h = new Node('a');
+// h.next = new Node('b');
+// h.next.next = new Node("c");
+
+// console.log(reverseLinkedList(h));
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
