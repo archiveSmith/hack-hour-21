b311da9d846f099b0eeaae8dcbef07a4e1c9edfa
Kyle <kaloftin@hotmail.com>
2018-05-16 09:45:22 -0700

    Merge Array First Commit

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -7,14 +7,19 @@
  * var another_array = [1,5,8,12,14,19];
  *
  * mergeArrays(my_array, another_array); -> [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21]
- * 
- * BONUS: 
- * Complete in O(n) time  
+ *
+ * BONUS:
+ * Complete in O(n) time
  *
  */
 
 function mergeArrays(arr1, arr2) {
-
+  const arr = arr1.concat(arr2);
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+  arr.sort(compareNumbers);
+  return arr;
 }
 
 module.exports = mergeArrays;
