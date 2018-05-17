6e94f7ce96f94936405789ce8c305046aa2039b3
James Tu <james@2-bit-toys.com>
2018-05-17 09:35:02 -0700

    apple stock brute force method

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -13,7 +13,33 @@
  */
 
 function bestProfit(stock_prices_yesterday) {
+  // check for invalid input
+  if (stock_prices_yesterday === null || stock_prices_yesterday === undefined) return 0;
+
+  let currBuy = -Infinity;
+  let currSell = -Infinity;
+  let currMaxProfit = -Infinity;
+
+  // find the lowest price and then the highest price?
+  for (let buy=0; buy<stock_prices_yesterday.length; buy++){ // loop through each price
+    currBuy = stock_prices_yesterday[buy];
+    if (currBuy < 0) return 0;
+    for(let sell=buy+1; sell<stock_prices_yesterday.length; sell++){
+      currSell = stock_prices_yesterday[sell];
+      if (currSell < 0) return 0;
+      if ( (currBuy - currSell) > currMaxProfit ) currMaxProfit = currBuy - currSell;
+    }
+  }
+
+  
+  return ( currMaxProfit > 0 ) ? currMaxProfit : 0;
 
 }
+
+// let s = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+// let s = [1,2,3,4,-10,-20];
+// // s.reverse();
+
+// console.log(bestProfit(s));
 
 module.exports = bestProfit;
