f0de3fdea97c9646d9c5727bcb2fa2ea9f3fb19c
Stacy Adelsberger <sladelsberger@gmail.com>
2018-05-17 09:58:03 -0700

    completed

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -13,7 +13,23 @@
  */
 
 function bestProfit(stock_prices_yesterday) {
-
-}
+    if (stock_prices_yesterday.length < 2 || !Array.isArray(stock_prices_yesterday)) {
+      return 0;
+    }
+    
+    let minPrice = stock_prices_yesterday[0];
+    let maxProfit =stock_prices_yesterday[1] - stock_prices_yesterday[0];
+    
+    for (var i = 1; i < stock_prices_yesterday.length; i++) {
+      let currentPrice = stock_prices_yesterday[i];
+      let potentialProfit = currentPrice - minPrice;
+      
+      maxProfit = Math.max(maxProfit, potentialProfit);
+      
+      minPrice = Math.min(minPrice, currentPrice);
+    }
+    
+    return maxProfit;
+  }
 
 module.exports = bestProfit;
