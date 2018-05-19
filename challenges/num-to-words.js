647a8cdcbae3dcdd9d21afca40f82c469fcf1cc5
Stacy Adelsberger <sladelsberger@gmail.com>
2018-05-19 09:14:54 -0700

    works up to 100

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -13,7 +13,45 @@
  */
 
 function numToWords(num) {
-
-}
+    let underTwenty = [
+      "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+    ];
+    let tens = [
+      "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+    ];
+    let scales = [
+      "Hundred", "Thousand", "Million", "Billion", "Trillion", "Quadrillion"
+    ];
+  
+    let words = [];
+  
+    if (num === 0) {
+      return 0;
+    }
+  
+    if (num < 20) {
+      return underTwenty[num - 1];
+    }
+  
+    if (num < 100) {
+      let isOnes = num % 10;
+      let isTens = Math.floor(num / 10);
+      words.push(tens[isTens - 1]);
+      if (isOnes !== 0) {
+        words.push(numToWords(isOnes));
+      }
+      return words.join("");
+    }
+  
+  }
+  
+  function thousandChunk(number) {
+    let thousands = [];
+    while (number > 0) {
+      thousands.push(number % 1000);
+      number = Math.floor(number / 1000);
+    }
+    return thousands;
+  }
 
 module.exports = numToWords;
