81eabcad5fd8f0e7b685c8697d430ae833296b4f
William LeGate <william@legate.me>
2018-05-21 09:53:30 -0700

    Completes perm-palin challenge

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,25 @@
  */
 
 function permPalin(str) {
-	
+  const count = {};
+  for (let i = 0; i < str.length; i += 1) {
+    const letter = str[i];
+    count[letter] = count[letter] ? count[letter] + 1 : 1;
+  }
+
+  let hasOddCount = false;
+  for (const key in count) {
+    const isOdd = count[key] % 2 > 0;
+    if (isOdd) {
+      if (hasOddCount) {
+        return false;
+      } else {
+        hasOddCount = true;
+      }
+    }
+  }
+
+  return true;
 }
 
 module.exports = permPalin;
