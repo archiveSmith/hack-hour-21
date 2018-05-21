453bc507daf475f04ac65649c9a1f29eb10ae411
Arman Bedrosyan <redarman@hotmail.com>
2018-05-21 09:59:53 -0700

    5/21 10 am

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -9,8 +9,21 @@
  *
  */
 
-function permPalin(str) {
-	
+const isPalin = (str1) => {
+  for (let i = 0; i < Math.floor(str1.length / 2); i++) {
+    if (str1[i] !== str1[str1.length - 1 - i]) return false;
+  }
+  return true;
+}
+
+
+function permPalin(str2) {
+  //run isPalin on pieces of str2
+  for (let i = 0; i < str2.length - 1; i++) {
+    console.log(str2.substring(i));
+    if(isPalin(str2.substring(i + 1))) return true;
+  }
+  return false;
 }
 
 module.exports = permPalin;
