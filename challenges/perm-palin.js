8bbbe108b768ebb9928de10f21efe9efe3b27b78
Jessica Skok <jessicaskok@gmail.com>
2018-05-21 11:06:21 -0700

    a few challenges

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -8,9 +8,22 @@
  * 	- permPalin('a') => true
  *
  */
-
+//palindrome - if string has even number of characters, it must have each of the characters exactly twice (or twice^)
+//if string has odd number of characters, it must have each of the charactres exactly twice (or twice^) plus one 
 function permPalin(str) {
-	
-}
+  let strContainer = 0;
+  let strArray = str.split('');
+  console.log(strArray);
+  if (str.length % 2 === 0);
+    for (let i = 0; i < strArray.length; i++){
+      count = 0;
+      for (let j = 0; j < strArray.length; j++){
+        if (strArray[j] === strArray[i]){
+          count++;
+        }
+      }
+    }
+  }
+
 
 module.exports = permPalin;
