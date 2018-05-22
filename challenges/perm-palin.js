2897e3d7e5ae8e6dce1367ba2b5bcac61d30b5a4
Colin McCarthy <colin@Colins-MacBook-Pro.local>
2018-05-22 10:08:15 -0700

    complete stack queue?

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,13 @@
  */
 
 function permPalin(str) {
-	
+  if (str.length < 2) return true;
+  return Object.values(str.split('').reduce((acc, e) => {
+    if (/\s/.test(e)) return acc;
+    if (!acc[e]) acc[e] = 1;
+    else acc[e] += 1;
+    return acc;
+  }, {})).filter(e => e % 2 !== 0).length <= 1;
 }
 
 module.exports = permPalin;
