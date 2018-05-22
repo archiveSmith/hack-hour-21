089da5663a793010e9b6a826d5745b32b653fa6d
William LeGate <william@legate.me>
2018-05-22 09:47:19 -0700

    Adds required method declatations to Stack() function

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -1,13 +1,38 @@
 /**
- * Create a stack.Then create a queue using two stacks.
+ * Create a stack. Then create a queue using two stacks.
+ * 
+ *
+ * [DONE] should have a push and pop method
+ * [DONE] should push elements to stack
+ * [DONE] should return undefined when popping empty stack
+ * [DONE] should have enqueue and dequeue method
+ * 
+ * [TODO] should push elements to queue
+ * [TODO] should return undefined when popping empty queue
+ * [TODO] should pop most recently pushed elements
+
  */
 
-function Stack() {}
+function Stack() {
+  const _stack = [];
+  return {
+    push: (...items) => {
+      _stack.push(...items);
+    },
+    pop: () => {
+      return _stack.pop();
+    },
+    enqueue: () => {},
+    dequeue: () => {}
+  };
+}
 
 /**
  * Queue Class
  */
 
-function Queue() {}
+function Queue() {
+  return [Stack(), Stack()];
+}
 
 module.exports = { Stack, Queue };
