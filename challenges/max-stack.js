31b840bdfec1787cf195c5a539cc9a406a484023
Kyle <kaloftin@hotmail.com>
2018-05-22 10:06:50 -0700

    Stack-Queue

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -5,35 +5,37 @@
  * getMax should return the largest value currently in the stack.
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
-
-function Stack() {
-  this.count = 0;
-  this.storage = {};
+class Stack {
+  constructor() {
+    this.count = 0;
+    this.storage = {};
+  }
+  push(elm) {
+    this.count += 1;
+    this.storage[this.count] = elm;
+  }
+  pop() {
+    if (this.count === 0)
+      return undefined;
+    this.count -= 1;
+    const pops = this.storage[this.count];
+    delete this.storage[this.count];
+    return pops;
+  }
+  getMax() {
+    let max = 0;
+    const vals = this.storage;
+    for (let prop in vals) {
+      max = (max < parseFloat(vals[prop])) ? parseFloat(vals[prop]) : max;
+    }
+    return max;
+  }
 }
 
-Stack.prototype.push = function (elm) {
-  this.storage[this.count] = elm;
-  this.count += 1;
-};
-
-Stack.prototype.pop = function () {
-  if (this.count === 0) return undefined;
-  this.count -= 1;
-  const pops = this.storage[this.count];
-  delete this.storage[this.count];
-  return pops;
-};
-
-Stack.prototype.getMax = function () {
-  let max = 0;
-  const vals = this.storage;
-  for (let prop in vals) {
-    max = (max < parseFloat(vals[prop])) ? parseFloat(vals[prop]) : max;
-  }
-  return max;
-};
 
 
+
+Stack.prototype.push(5);
 
 
 module.exports = Stack;
