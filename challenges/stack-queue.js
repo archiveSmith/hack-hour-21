aa212dc38486f2930306cbf74c727bb3218e59b5
Jeff <jeffreyrkang@gmail.com>
2018-05-22 09:29:59 -0700

    Complete stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,13 @@
 
 
 function Stack() {
-
+  this.store = [];
+  this.push = function (data) {
+    this.store.push(data);
+  };
+  this.pop = function (data) {
+    return this.store.pop();
+  }
 }
 
 
@@ -14,7 +20,30 @@ function Stack() {
 
 
 function Queue() {
-
+  this.in = new Stack();
+  this.out = new Stack();
+  this.queue = function (data) {
+    this.in.push(data);
+  };
+  this.dequeue = function (data) {
+    if (this.in.store.length === 0 && this.out.store.length === 0) return undefined;
+    if (this.out.store.length > 0) {
+      return this.out.pop();
+    } else {
+      while(this.in.store.length > 0) {
+        this.out.push(this.in.pop());
+      }
+      return this.out.pop();
+    }
+  };
 }
+
+// const q = new Queue();
+// q.queue(1);
+// q.queue(2);
+// q.queue(3);
+// console.log(q.dequeue());
+// console.log(q.dequeue());
+// console.log(q.dequeue());
 
 module.exports = {Stack: Stack, Queue: Queue};
