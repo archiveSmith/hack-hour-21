ac1077a7ebfe61003063250f5b68bdfe7c948856
James Tu <james@2-bit-toys.com>
2018-05-23 09:43:17 -0700

    two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -4,6 +4,33 @@
 
 function twoSum(arr, n) {
 
+  let sortedArr = [...arr].sort();
+  // console.log(sortedArr);
+
+  let lowIdx = 0;
+  let highIdx = sortedArr.length-1;
+
+  // Because this is sorted, we'll never be able to make up that sum
+  if (sortedArr[lowIdx] + sortedArr[highIdx] < n) return false;
+
+  while(lowIdx < highIdx) {
+    // console.log(`${sortedArr[lowIdx]} + ${sortedArr[highIdx]}`);
+    if (sortedArr[lowIdx] + sortedArr[highIdx] === n) return true;
+    if (sortedArr[lowIdx] + sortedArr[highIdx] > n) {
+        highIdx--;
+      } else {
+        lowIdx++;
+      }
+  }
+
+  return false;
+
 }
+
+// console.log(twoSum([1,1,1,1,1,1,2,1,3], 2));
+// console.log(twoSum([1,-1,1,1,1,1,2,1,0], -1));
+
+
+
 
 module.exports = twoSum;
