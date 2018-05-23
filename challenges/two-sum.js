ebbfdc5c71ff95e0eb12886bd67161d97ef60c17
Jeff <jeffreyrkang@gmail.com>
2018-05-23 09:26:20 -0700

    Complete two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,16 @@
  */
 
 function twoSum(arr, n) {
-
+  if (!Array.isArray(arr)) return false;
+  if (arr.length < 2) return false;
+  for (let i = 0; i < arr.length; i += 1) {
+    for (let j = i + 1; j < arr.length; j += 1) {
+      if (arr[i] + arr[j] === n) return true;
+    }
+  }
+  return false;
 }
+
+// console.log(twoSum([-1,2,4,6], 5));
 
 module.exports = twoSum;
