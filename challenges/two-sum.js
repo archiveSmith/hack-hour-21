23cd5aa3c00db716c7502b9160f3a2c1cd549837
Jessica Skok <jessicaskok@gmail.com>
2018-05-23 09:41:06 -0700

    two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,14 @@
  */
 
 function twoSum(arr, n) {
-
+  for (let i = 0; i < arr.length; i++){
+    for (let j = 1; j < arr.length; j++){
+      if (arr[i] + arr[j] === n){
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 module.exports = twoSum;
