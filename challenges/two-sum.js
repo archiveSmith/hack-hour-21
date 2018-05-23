d945e7c3df0d7aced41652c94406b72b738a0a51
Stefan <stefan.armijo@gmail.com>
2018-05-23 09:45:21 -0700

    implement two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,20 @@
  */
 
 function twoSum(arr, n) {
-
+    for (let i = 0; i < arr.length; i += 1) {
+        for (let j = i + 1; j < arr.length; j += 1) {
+            if (arr[i] + arr[j] === n) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
+// const a = [1, 2, 3, 4, 5, 5];
+
+// console.log(twoSum(a, 10));
+// console.log(twoSum(a, 2));
+
 module.exports = twoSum;
+
