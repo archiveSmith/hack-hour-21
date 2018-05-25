3c84d117a9619e5297e30f561c05f9894b5f0e36
Jeff <jeffreyrkang@gmail.com>
2018-05-25 09:35:30 -0700

    Complete gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,8 +7,35 @@
  *
  */
 
-function gcd(a, b) {
-
+function union(a, b) {
+  const output = {};
+  for (var key in b) {
+    if (a.hasOwnProperty(key)) output[key] = b[key];
+  }
+  return output;
 }
+
+function gcd(a, b) {
+  const aDiv = {};
+  const bDiv = {};
+  for (let i = 1; i < a; i += 1) {
+    if (a % i === 0) {
+      aDiv[i] = ' ';
+    }
+  }
+  for (let j = 1; j < b; j += 1) {
+    if (b % j === 0) {
+      bDiv[j] = ' ';
+    }
+  }
+  const merge = union(aDiv, bDiv);
+  let max = -Infinity;
+  for (var key in merge) {
+    if (Number(key) > max) max = Number(key);
+  }
+  return max;
+}
+
+// console.log(gcd(20, 25));
 
 module.exports = gcd;
