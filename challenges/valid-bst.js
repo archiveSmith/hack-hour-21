eaec1a5fe62206c60a70dd034a01312777d404e3
Stefan <stefan.armijo@gmail.com>
2018-05-25 09:47:47 -0700

    progress on valid-bst

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -6,14 +6,56 @@
  */
  
 
-function BinaryTree(val) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
+function Node(val) {
+  this.value = val;
+  this.left = null;
+  this.right = null;
 }
+
+function BinaryTree() {
+    this.root = null;
+};
+
+BinaryTree.prototype.push = function (val) {
+  var root = this.root;
+
+  if (!root) {
+    this.root = new Node(val);
+    return;
+  }
+
+  var currentNode = root;
+  var newNode = new Node(val);
+
+  while (currentNode) {
+    if (val < currentNode.value) {
+      if (!currentNode.left) {
+        currentNode.left = newNode;
+        break;
+      }
+      else {
+        currentNode = currentNode.left;
+      }
+    }
+    else {
+      if (!currentNode.right) {
+        currentNode.right = newNode;
+        break;
+      }
+      else {
+        currentNode = currentNode.right;
+      }
+    }
+  }
+
+}
+
 
 function validBST(tree) {
 
+  let root = tree;
+
 }
 
-module.exports = {BinaryTree: BinaryTree, validBST: validBST};
+module.exports = { BinaryTree: BinaryTree, validBST: validBST };
+
