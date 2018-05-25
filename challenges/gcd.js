ab990856af945b30f666a31523156bd166948a7e
William LeGate <william@legate.me>
2018-05-25 10:12:11 -0700

    Completes GCD challenge

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,18 @@
  */
 
 function gcd(a, b) {
+  const aDivisors = divisors(a);
+  const bDivisors = divisors(b);
+  const commonDivisors = aDivisors.filter(x => bDivisors.indexOf(x) !== -1);
+  return commonDivisors.pop();
+}
 
+function divisors(x) {
+  const divisors = [];
+  for (let i = 1; i <= x; i += 1) {
+    if ((x / i) % 1 === 0) divisors.push(i);
+  }
+  return divisors;
 }
 
 module.exports = gcd;
