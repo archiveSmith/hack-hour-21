22581106b9daecd1c60f35e88c8c0013d411c344
William Adamowicz <william.adamowicz@gmail.com>
2018-06-05 10:14:24 -0700

    complete add linked list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -18,7 +18,60 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  let carry = false;
+  let nums1 = [];
+  let nums2 = [];
+  let result = [];
+  let current;
+  
+  while (l1) {
+    nums1.push(l1.value);
+    l1 = l1.next;   
+  }
+  
+  while (l2) {
+    nums2.push(l2.value);
+    l2 = l2.next;   
+  }
+  
+  let longest = nums1.length > nums2.length ? nums1 : nums2;
+  let shortest = nums1 === longest ? nums2 : nums1;
+  
+  longest.reverse();
+  shortest.reverse();
+  
+  for (let i = 0, sum; i < longest.length; i++) {
+    sum = longest[i] + shortest[i];
+    if (sum < 10) {
+      result.push(carry ? sum + 1 : sum)
+      carry = false;
+    } else {
+      result.push(sum % 10)
+      carry = true;
+    }
+  }
+  
+  let resultList = new Node(result[0]);
+  let first = resultList;
+  result.shift();
+  
+  for (let j = 0; j < result.length; j++) {
+    resultList.next = new Node(result[j]);
+    resultList = resultList.next;
+  }
 
+  return first;
 }
+
+// l1 = new Node(2);
+// l1.next = new Node(1);
+// l1.next.next = new Node(5);
+
+// l2 = new Node(5);
+// l2.next = new Node(9);
+// l2.next.next = new Node(2);
+
+// addLinkedList(l1, l2);
+
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
