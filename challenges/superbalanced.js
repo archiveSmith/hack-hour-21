6e8cbed3188b1c0d4c7924538e29b4e738a6ff36
Kyle <kaloftin@hotmail.com>
2018-06-06 09:54:34 -0700

    Superbalanced Tree

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -6,15 +6,37 @@
 3) The difference between heights of left subtree and right subtree is not more than 1.
 example: http://www.geeksforgeeks.org/wp-content/uploads/balanced_tree.GIF
  */
-
-function BinaryTree(value) {
-  this.value = value;
-  this.left = null;
-  this.right = null;
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+  insertLeft(value) {
+    this.left = new BinaryTree(value);
+    return this.left;
+  }
+  insertRight(value) {
+    this.right = new BinaryTree(value);
+    return this.right;
+  }
 }
 
-function superbalanced(tree) {
 
+function superbalanced(node, depth = 0, depths = []) {
+  if (!node.left && !node.right) {
+    if (depths.indexOf(depth) < 0) {
+      depths.push(depth);
+
+      if (depths.length > 2 || (depths.length === 2 && (Math.abs(depths[0] - depths[1]) > 1))) {
+        return false;
+      }
+    }
+  } else {
+    return superbalanced(node.left, depth + 1, depths) && superbalanced(node.right, depth + 1, depths);
+  }
+
+  return true;
 }
 
-module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
+module.exports = { BinaryTree, superbalanced };
