5abd4cf09611ac937eb661739b999777313c0616
Jessica Skok <jessicaskok@gmail.com>
2018-06-07 09:31:50 -0700

    repeat number

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -9,9 +9,21 @@
  * Do this in O(1) space
  *
  */
+//create a variable to store number
+//for loop through array, assigning number to variable and checking to see if following numbers equal that one (2 for loops?)
 
 function repeatNumbers(array) {
+  let possibleDouble = 0;
 
+  for (let i = 0; i < array.length; i++){
+    possibleDouble = array[i];
+    for (let z = i + 1; z < array.length; z++){
+      if (array[z] === possibleDouble){
+        return possibleDouble;
+      }
+    }
+  }
+  return 'no double!';
 }
 
 module.exports = repeatNumbers;
