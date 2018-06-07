b4f8f4c777c2182f9e339fb551f7374246bd285e
Arman Bedrosyan <redarman@hotmail.com>
2018-06-07 09:47:45 -0700

    6/7

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -9,9 +9,43 @@
  * Do this in O(1) space
  *
  */
+//double for loop
+//time:
+//space
+// function repeatNumbers(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[i] === array[j]) return array[i];
+//     }
+//   }
 
+//   return undefined;
+// }
+
+//single for loop
+//time:
+//space:
 function repeatNumbers(array) {
+  if(!Array.isArray(array)) return undefined;
 
+  let obj = {};
+  
+  for (let i = 0; i < array.length; i++) {
+    if (obj[array[i]] === 1) return array[i];
+    else (obj[array[i]] = 1);
+  }
+
+  return undefined;
 }
+
+// function repeatNumbers(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[i] === array[j]) return array[i];
+//     }
+//   }
+
+//   return undefined;
+// }
 
 module.exports = repeatNumbers;
