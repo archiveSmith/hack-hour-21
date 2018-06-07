635bb92146b8528d74041a9d3431c49c6805b896
James Tu <james@2-bit-toys.com>
2018-06-07 10:33:43 -0700

    brute force O(n) time and space

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -10,8 +10,29 @@
  *
  */
 
-function repeatNumbers(array) {
-
+// Brute force. O(n) time,  O(n) space
+function repeatNumbersBrute(array) {
+  let occurs = {};
+  for(let i=0; i<array.length; i++){
+    if (occurs[array[i]]) {
+      return array[i]
+    } else {
+      occurs[array[i]] = 'y';
+    }
+  }
 }
+
+
+// function repeatNumbersTrick(array) {
+//   let factorial = array.reduce( (elem, 1 ) => { elem * curr );
+//   console.log(factorial);
+
+//   // let dup = factorial / 
+
+// }
+
+
+
+// console.log(repeatNumbers([1,2,11,3,4,5,6,7,8,9,11,100,24]));
 
 module.exports = repeatNumbers;
