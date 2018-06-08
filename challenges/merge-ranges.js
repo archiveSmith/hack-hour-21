712a3d0c1da17d15ca512328ca42fa0910b3f696
Will Howard <Will@Wills-MacBook-Pro.local>
2018-06-08 09:32:30 -0700

    merge range

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,17 @@
 
 
 function mergeRanges(array) {
-
+  let resultArr = [], last;
+  array.forEach((ele) => {
+      if (!last || ele[0] > last[1]) {
+        resultArr.push(last = ele);
+      } else if (ele[1] > last[1]) {
+        last[1] = ele[1];
+      }
+  });
+  return resultArr;
 }
 
+var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]];
+console.log(mergeRanges(times));
 module.exports = mergeRanges;
