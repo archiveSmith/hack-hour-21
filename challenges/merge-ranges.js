1f0ef75a6ea32193c00b6e88b0b386a81e8c95a4
Kyle <kaloftin@hotmail.com>
2018-06-08 09:33:45 -0700

    merge-range-done

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,19 @@
 
 
 function mergeRanges(array) {
-
+  let sortedArrs = [];
+  for (let j = 0; j < array.length; j++) {
+    sortedArrs.push(array[j].sort(function(a, b) {return a[0] - b[0]}));
+  }
+  let returnArr = [array[0].sort()];
+  for (let i = 1; i < array.length; i++) {
+    if (returnArr[returnArr.length-1][1] >= array[i].sort()[0]) {
+    returnArr[returnArr.length-1] = [returnArr[returnArr.length-1][0],array[i].sort()[1]];
+    } else {
+      returnArr.push(array[i]);
+    }
+  }
+    return returnArr;
 }
 
 module.exports = mergeRanges;
