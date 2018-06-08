5794b17c11066dcc9636444b059196e65a1cc1c0
Christopher <chris132128@gmail.com>
2018-06-08 16:30:51 -0700

    merge-ranges notes

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -9,9 +9,26 @@
  * Do not assume the ranges are in order
  */
 
+//  [4, 8], [3, 5]
 
 function mergeRanges(array) {
-
+  return array.reduce((acc, next, index, arr) => {
+    console.log(`acc: ${acc}`);
+    // console.log(`index: ${index}`);
+    // console.log(`arr: ${arr}`);
+    acc.forEach(ele => {
+      if (ele[1] >= next[0] && ele[1] < next[1]) {
+        ele[1] = next[1];
+        // return acc;
+      } else if (ele[0] > next[0] && ele[1] > next[1]) {
+        // return acc;
+      } else acc.push(next);
+    });
+    // acc.push(next);
+    return acc;
+  }, []);
 }
+
+console.log(mergeRanges([[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]));
 
 module.exports = mergeRanges;
