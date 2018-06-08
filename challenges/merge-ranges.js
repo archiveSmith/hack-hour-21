57dff4764188d7fbba004d54c8db109fedf29578
Jeff <jeffreyrkang@gmail.com>
2018-06-08 09:31:09 -0700

    Complete merge-ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,43 @@
 
 
 function mergeRanges(array) {
-
+  const obj = {};
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = array[i][0]; j <= array[i][1]; j += 1) {
+      obj[j] = ' ';
+    }
+  }
+  console.log(obj);
+  let count = 0;
+  const output = [];
+  let range = [];
+  while (Object.keys(obj).length !== 0) {
+    if (obj.hasOwnProperty(count)) {
+      if (range.length === 0) {
+        range.push(count);
+        delete obj[count];
+        count += 1;
+      } else {
+        delete obj[count];
+        count += 1;
+      }
+    } else if (range.length === 1) {
+      range.push(count - 1);
+      output.push(range);
+      range = [];
+      count += 1;
+    } else {
+      count += 1;
+    }
+  }
+  if (range.length === 1) {
+    range.push(count - 1);
+    output.push(range);
+  }
+  return output;
 }
+
+// const times = [[0, 1], [3, 5], [4, 7], [10, 12], [9, 10]];
+// console.log(mergeRanges(times)); // [[0, 1], [3, 8], [9, 12]]
 
 module.exports = mergeRanges;
