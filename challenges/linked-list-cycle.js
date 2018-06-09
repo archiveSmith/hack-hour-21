de55b8f1597807f1c1e6cae8ae139542c0f551fc
Christopher <chris132128@gmail.com>
2018-06-09 09:50:00 -0700

    linked-list-cycle

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -33,7 +33,27 @@ var Node = function(value) {
 }
 
 function hasCycle(head) {
-
+  let turtle = head.next;
+  let bunny = head.next.next;
+  while (turtle !== bunny && turtle && bunny) {
+    // console.log(`bunny: ${bunny}, turtle: ${turtle}`)
+    // console.log(turtle);
+    // console.log(bunny);
+    turtle = turtle.next;
+    if (bunny.next) bunny = bunny.next.next;
+    else bunny = bunny.next;
+    if (turtle === bunny) return true;
+  }
+  return false;
 }
+
+var node1 = new Node('1');
+var node2 = node1.next = new Node('2');
+var node3 = node2.next = new Node('3');
+var node4 = node3.next = new Node('4');
+var node5 = node4.next = new Node('5');
+hasCycle(node1); // => false
+node5.next = node2;
+hasCycle(node1); // => true
 
 module.exports = {Node: Node, hasCycle: hasCycle}
