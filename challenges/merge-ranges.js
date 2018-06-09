7c47595f14c7cc0c7a8cb9bff04db71a70adaf1e
Stacy Adelsberger <sladelsberger@gmail.com>
2018-06-09 08:53:59 -0700

    merged ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -12,6 +12,39 @@
 
 function mergeRanges(array) {
 
+var swapped;
+
+do {
+  swapped = false;
+  
+  for( var i = 0; i < array.length - 1; i++){
+    if(array[i][0] > array[i+1][0]) {
+      var temp = array[i];
+      array[i] = array[i + 1];
+      array[i+1] = temp;
+      swapped = true;
+    }
+  }
+  } while(swapped)
+
+//console.log(array)
+
+var newAry = [];
+
+for( var j = 0; j < array.length-1; j++){
+  
+  //console.log(array[j][1], array[j+1][0])
+  
+  if(array[j][1] >= array[j+1][0]){
+    newAry.push([array[j][0],array[j+1][1]])
+    j++
+  } else {
+    newAry.push(array[j])
+  }
+}
+
+// console.log(newAry)
+return newAry
 }
 
 module.exports = mergeRanges;
