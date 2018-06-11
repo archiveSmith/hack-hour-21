e4985bc3bc2f8ba2e02f5dcbde3fb80faa9536b1
Arman Bedrosyan <redarman@hotmail.com>
2018-06-11 10:07:26 -0700

    fix

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -7,8 +7,8 @@
 
 function sumMultiples3Or5Below1000() {
   let sum = 0;
-  for (let i = 0; i <= 1000; i++) {
-    if (i % 3 || i % 5) sum += i;
+  for (let i = 0; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) sum += i;
   }
   return sum;
 }
@@ -18,8 +18,8 @@ function sumMultiples3Or5Below1000() {
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
   let sum = 0;
-  for (let i = 0; i <= z; i++) {
-    if (i % x || i % y) sum += i;
+  for (let i = 0; i < z; i++) {
+    if (i % x === 0 || i % y === 0) sum += i;
   }
   return sum;
 }
