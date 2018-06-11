4d7a05b9f46bcc495920e64e69e8d9fbd118bd8b
Will Howard <Will@Wills-MacBook-Pro.local>
2018-06-11 09:30:04 -0700

    multiples of 3 or 5

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -7,7 +7,15 @@
 
 function sumMultiples3Or5Below1000() {
   let sum = 0;
-
+  // since we know there are at most 333 multiples of 3
+  let threes = Array.apply(null, {length: 333}).map(Number.call, Number);
+  threes = threes.map((ele => ele * 3));
+  // since we know there are at most 200 multiples of 5
+  let fives = Array.apply(null, {length: 200}).map(Number.call, Number);
+  fives = fives.map((ele => ele * 5));
+  let sumThrees = threes.reduce((sum, val) => {return sum + val});
+  let sumFives = fives.reduce((sum, val) => {return sum + val});
+  sum = sumThrees + sumFives;
   return sum;
 }
 
@@ -16,6 +24,14 @@ function sumMultiples3Or5Below1000() {
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
   let sum = 0;
+  let xs = Array.apply(null, {length: Math.floor(z / x)}).map(Number.call, Number);
+  xs = xs.map(ele => ele * x);
+
+  let ys = Array.apply(null, {length: Math.floor(z / y)}).map(Number.call, Number);
+  ys = ys.map(ele => ele * y);
+  let sumX = xs.reduce((sum, val) => {return sum + val});
+  let sumY = ys.reduce((sum, val) => {return sum + val});
+  sum = sumX + sumY;
 
   return sum;
 }
