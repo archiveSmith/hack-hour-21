69fefbe17e5457cc85adebd225d425c1b2dc8af7
William Adamowicz <william.adamowicz@gmail.com>
2018-06-12 09:18:47 -0700

    complete maxsub

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,22 @@
  */
 
 function maxSubarray(arr) {
-
+    subs = [];
+    let num = arr.length; 
+    while (num--) {
+        for (let i = 0; i < arr.length; i++) {
+            subs.push(arr.slice(i));
+        }
+            arr.pop();
+    }
+    return Math.max(...subs.map(getSum));
 }
+
+function getSum(arr) {
+    return arr.reduce((a, b) => a + b);
+}
+
+// console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5])) 
+// console.log(maxSubarray([15,20,-5,10])) 
 
 module.exports = maxSubarray;
