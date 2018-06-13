b8ccb5a213cb5dad58e83ea55e9f3fb277c5ef6c
Stacy Adelsberger <sladelsberger@gmail.com>
2018-06-13 09:44:13 -0700

    using permutations

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,20 @@
   */
 
 function anagrams(string) {
+  if (string.length < 2) return string;
 
+    var permutations = [];
+
+    for (var i=0; i<string.length; i++) {
+        var char = string[i];
+
+        var remainingString = string.slice(0,i) + string.slice(i+1,string.length); 
+
+        for (var subPermutation of anagrams(remainingString))
+            permutations.push(char + subPermutation)
+
+    }
+    return permutations;
 }
 
 module.exports = anagrams;
