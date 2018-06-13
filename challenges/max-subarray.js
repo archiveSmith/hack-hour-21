1fb0c17128aad240a1baf24fff5f23efe80de716
James Tu <james@2-bit-toys.com>
2018-06-13 11:07:07 -0700

    max subarray

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,26 @@
  */
 
 function maxSubarray(arr) {
+  
+  if ( arr.length === 1) return arr[0];
+  if ( !arr || arr.length === 0 ) return null;
+  
+  let max = -Infinity;
+  let sum = 0;
+  for (let i=0; i<arr.length; i++){
+    let sum = arr[i];
+    for (let j=i+1; j<arr.length; j++){
+      sum += arr[j];
+      max = Math.max(sum, max);
+    }
+  }
 
+  return max;
+  
 }
+
+console.log(maxSubarray([5,2,3,-1,4,1]));
+console.log(maxSubarray([15,20,-5,10]));
+
 
 module.exports = maxSubarray;
