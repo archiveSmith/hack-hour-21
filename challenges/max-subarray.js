7047b3bd7af2ee15d4503bebcf4eee7db10d4457
Stacy Adelsberger <sladelsberger@gmail.com>
2018-06-13 08:40:54 -0700

    used Kadane's algorithm

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,19 @@
  */
 
 function maxSubarray(arr) {
+    let maxSoFar = 0;
+    let currentMax = 0;
 
+    for (let i = 0; i < arr.length; i++) {
+        currentMax = currentMax + arr[i];
+        if (currentMax < 0) {
+            currentMax = 0;
+        }
+        if (maxSoFar < currentMax) {
+            maxSoFar = currentMax;
+        }    
+    }
+    return maxSoFar;
 }
 
 module.exports = maxSubarray;
