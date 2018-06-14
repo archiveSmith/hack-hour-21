936ba71da58b56af4b2859dc7b20464e8f8239c0
Tiffany Yang <teefyang@Tiffanys-MacBook-Pro.local>
2018-06-14 10:02:15 -0700

    anagrams completed

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,32 @@
   */
 
 function anagrams(string) {
+  //for each letter in string, loop over the same string
+  //if the letter is not the same as the iterable, then form concat the string and collect them
+  // taking each of the collection, loop through them and compare with each letter in string
+    //if not found, then concat the collection item to the letter compared 
+  let collection = [];
+  let result = [];
+  let arraystring = string.split("");
+  for(let i = 0; i<arraystring.length; i++){
+    for(let a = 0; a<string.length; a++){
+      if(arraystring[i] !== string[a]){
+        collection.push(arraystring[i].concat(string[a]))
+      }
+    }
+  }
+  for(let c = 0; c<string.length; c++){
+    for(let e = 0; e<collection.length; e++){
+      if(!collection[e].includes(string[c])){
+        result.push(collection[e].concat(string[c]))
+      }
+    }
+  }
+  return result;
 
 }
+var result = anagrams('abc');
+console.log(result);
 
 module.exports = anagrams;
+
