81fac1d7577708c3fecca20a05f55b64d7304ff2
Colin McCarthy <colin@Colins-MacBook-Pro.local>
2018-06-14 09:59:33 -0700

    completes delete-dups

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,32 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
-
+var Node = function(value) {
+  this.value = value;
+  this.next = null;
+}
 
 function deleteDups(head) {
-
+  if (!head || !head.next) return head;
+  let currentNode = head;
+  let compareNode = head.next;
+  let chaser = currentNode;
+  while (currentNode.next) {
+    while (compareNode) {
+      // check inner loop node against outer loop node
+      if (currentNode.value === compareNode.value) {
+        // what if dupes are at the end?
+        if (!compareNode.next) chaser.next = null;
+        // drop the dupe and don't advance the chaser
+        else chaser.next = compareNode.next;
+      } else chaser = chaser.next;
+      compareNode = compareNode.next;
+    }
+    currentNode = currentNode.next;
+    compareNode = head;
+    chaser = head;
+  }
+  return head;
 }
 
 module.exports = deleteDups;
