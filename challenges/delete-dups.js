b21ce930361b192214f8e6d5b642f6bece2056e7
James Tu <james@2-bit-toys.com>
2018-06-14 09:23:51 -0700

    delete dups with buffer

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,43 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
-
+function Node (val) {
+  this.value = val;
+  this.next = null;
+}
 
 function deleteDups(head) {
+  let seen = {};
+  let nextNode = head;
+  let prevNode = null;
+  let savedNext;
+  while (nextNode){
+    if (seen[nextNode.value]) { // if we have already seen it
+      // remove it by adjusting the pointers
+      prevNode.next = nextNode.next;
+      // make sure the remove node doesn't ref any other nodes so it can be garbage collected 
+      nextNode.next = null;
+
+      // set it up for the next iteration 
+      nextNode = prevNode.next;
+    } else {
+      seen[nextNode.value] = 'hi';
+
+      // set it up for the next iteration
+      prevNode = nextNode; // save this node
+      nextNode = nextNode.next;
+    }
+
+  }
+
+  return head;
 
 }
+
+// let h = new Node(2);
+// h.next = new Node(2);
+// h.next.next = new Node(3);
+
+// console.log(deleteDups(h));
 
 module.exports = deleteDups;
