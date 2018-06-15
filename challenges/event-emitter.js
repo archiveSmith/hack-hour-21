d49a0a72076ece7df77963ccd8e931c8b30e51aa
William Adamowicz <william.adamowicz@gmail.com>
2018-06-15 09:46:57 -0700

    handle multiple events

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -26,22 +26,23 @@ function EventEmitter() {
 }
 
 EventEmitter.prototype.on = function(funcName, func) {
-    this.events[funcName] = func;
+    if (this.events[funcName]) this.events[funcName].push(func);
+    else this.events[funcName] = [func]; 
 };
 
 EventEmitter.prototype.trigger = function(funcName, ...args) {
-    if (this.events[funcName]) this.events[funcName](args)
+    this.events[funcName].forEach(event => event(...args))
 };
 
- var instance = new EventEmitter();
- var counter = 0;
- instance.on('increment', function() {
-   counter++;
-}); // counter should be 0
-console.log(counter)
- instance.trigger('increment'); // counter should be 1
- console.log(counter)
- instance.trigger('increment'); // counter should be 2
- console.log(counter)
+//  var instance = new EventEmitter();
+//  var counter = 0;
+//  instance.on('increment', function() {
+//    counter++;
+// }); // counter should be 0
+// console.log(counter)
+//  instance.trigger('increment'); // counter should be 1
+//  console.log(counter)
+//  instance.trigger('increment'); // counter should be 2
+//  console.log(counter)
 
 module.exports = EventEmitter;
