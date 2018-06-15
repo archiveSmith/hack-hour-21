60e629722f5db79946ad182396742963573dc3b3
Christopher <chris132128@gmail.com>
2018-06-15 09:21:58 -0700

    event-emitter

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -22,15 +22,24 @@
  */
 
 function EventEmitter() {
-
+  this.cache = {};
 }
 
 EventEmitter.prototype.on = function(funcName, func) {
-
+  if (!this.cache[funcName]) this.cache[funcName] = [func];
+  else this.cache[funcName].push(func);
 };
 
 EventEmitter.prototype.trigger = function(funcName, ...args) {
-
+  this.cache[funcName].forEach(ele => ele(...args));
 };
+
+// var instance = new EventEmitter();
+// var counter = 0;
+// instance.on('increment', function() {
+//   counter++;
+// }); // counter should be 0
+// instance.trigger('increment'); // counter should be 1
+// instance.trigger('increment'); // counter should be 2
 
 module.exports = EventEmitter;
