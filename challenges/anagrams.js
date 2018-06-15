b18f3deed47f6dee70e1eee11ca0b8523c172b96
James Tu <james@2-bit-toys.com>
2018-06-15 09:35:37 -0700

    anagram

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,34 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
 
+// https://stackoverflow.com/questions/5232295/is-there-any-pre-built-method-for-finding-all-permutations-of-a-given-string-in-j
+
+function anagrams(str, index, buffer) {
+    if (typeof str == "string")
+        str = str.split("");
+    if (typeof index == "undefined")
+        index = 0;
+    if (typeof buffer == "undefined")
+        buffer = [];
+    if (index >= str.length)
+        return buffer;
+    for (var i = index; i < str.length; i++)
+        buffer.push(ToggleLetters(str, index, i));
+        
+    return anagrams(str, index + 1, buffer);
 }
+
+function ToggleLetters(str, index1, index2) {
+    if (index1 != index2) {
+        var temp = str[index1];
+        str[index1] = str[index2];
+        str[index2] = temp;
+    }
+    return str.join("");
+}
+
+// console.log(anagrams('hell'));
+
 
 module.exports = anagrams;
