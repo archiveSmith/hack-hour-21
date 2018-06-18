3a5e49a173097bfa4714171332bc9fd335aeae9c
Kyle <kaloftin@hotmail.com>
2018-06-18 09:51:07 -0700

    insertion-sort-done

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -3,9 +3,17 @@
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 
-
 function insertionSort(array) {
-
+  for (let i = 0; i < array.length; i += 1) {
+    const tempArr = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > tempArr) {
+      array[j + 1] = array[j];
+      j -= 1;
+    }
+    array[j + 1] = tempArr;
+  }
+  return array;
 }
 
 module.exports = insertionSort;
