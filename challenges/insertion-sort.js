72e1a23bcbdffb2df8ee38ffd199d5c2bccfb265
Stefan <stefan.armijo@gmail.com>
2018-06-18 09:45:45 -0700

    implement insertion sort based on Rosettacode psuedo code

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,20 @@
 
 
 function insertionSort(array) {
-
+   for (let i = 1; i < array.length; i += 1) {
+       let value = array[i];
+       let j = i - 1;
+       while (j >= 0 && array[j] > value) {
+           array[j + 1] = array[j];
+           j = j - 1;
+       }
+       array[j + 1] = value
+    }
+    // console.log(array);
 }
+
+// let array = [5, 2, 8, 33, 10];
+
+// insertionSort(array);
 
 module.exports = insertionSort;
