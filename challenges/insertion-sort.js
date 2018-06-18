094e889dfcb749910ebc0e7e68887a431b6c0dcc
Colin McCarthy <colin@Colins-MacBook-Pro.local>
2018-06-18 09:51:45 -0700

    complete insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,18 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
-
 function insertionSort(array) {
-
+  if (array.length < 2) return array;
+  for (let i = 1; i < array.length; i++) {
+    let temp = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > temp) {
+      array[j + 1] = array[j];
+      j -= 1; 
+    }
+    array[j + 1] = temp;
+  }
+  return array;
 }
 
 module.exports = insertionSort;
