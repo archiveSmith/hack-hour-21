52a95c6c0428f6f0268a4728d4f1b1802874767a
Stacy Adelsberger <sladelsberger@gmail.com>
2018-06-19 09:13:45 -0700

    using a temp variable

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,20 @@
 
 
 function insertionSort(array) {
-
+    for (let i = 1; i < array.length; i++) {
+      let temp;
+      let prev = array[i - 1];
+      let curr = array[i];
+        if (array[i] < array[i - 1]) {
+            temp = prev;
+            prev = curr;
+            curr = temp;
+        }
+    }
+    return array;
 }
+
+// console.log(insertionSort([3, 7, 4, 2]));
+
 
 module.exports = insertionSort;
