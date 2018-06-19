f2e58f425b68801be63e6be489e3b7ffbdc6a33b
Will Howard <Will@Wills-MacBook-Pro.local>
2018-06-19 09:43:41 -0700

    XOR is my overlord

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,17 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i++) {
+    let j = i - 1;
+    while (j >= 0 && array[j] > array[i]) {
+      let temp = arr[j];
+      array[j] = array[i];
+    }
+    array[i] = temp;
+  }
 }
+
+let test = [6, 3, 2, 5, 4, 1];
+console.log(insertionSort(test));
 
 module.exports = insertionSort;
