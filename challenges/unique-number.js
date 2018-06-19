2155f82b9c107cc9ecccc6149baad95effb36a7b
Christopher <chris132128@gmail.com>
2018-06-19 09:37:57 -0700

    unique num bitwise

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -10,11 +10,12 @@
  *
  */
 function uniqueNumber(array) {
-  const cache = {};
-  array.forEach(ele => cache[ele] ? delete cache[ele] : cache[ele] = 1);
-  return Object.keys(cache)[0];
+  // const cache = {};
+  // array.forEach(ele => cache[ele] ? delete cache[ele] : cache[ele] = 1);
+  // return Object.keys(cache)[0];
+  return array.reduce((a, b) => a ^ b);
 }
 
-console.log(uniqueNumber([1,2,1,3,3])); // -> 2
+// console.log(uniqueNumber([1,2,1,3,3])); // -> 2
 
 module.exports = uniqueNumber;
