2794cfa2618b4fee36548ba6e4bd3537bacf48d6
James Tu <james@2-bit-toys.com>
2018-06-19 10:12:18 -0700

    unique number XOR trick

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -9,8 +9,29 @@
  * Complete the challenge in O(1) space
  *
  */
-function uniqueNumber(array) {
-
+function uniqueNumber_brute(array) {
+  let seen = {};
+  
+  array.forEach(elem => {
+    if ( seen[elem] ) {
+      seen[elem] += 1;
+    } else {
+      seen[elem] = 1;
+    }
+  })
+  
+  for (k in seen) {
+    if ( seen[k] === 1 ) return k;
+  }
+  
 }
+
+// use XOR
+// any value that is XOR'ed with itself is 0
+function uniqueNumber(array) {
+ return array.reduce((acc,curr) => acc^curr)
+}
+
+// console.log(uniqueNumber([1,19,1,3,3]));
 
 module.exports = uniqueNumber;
