0d989f0bb04094582ffe9d620e49997c1cdd467e
Kyle <kaloftin@hotmail.com>
2018-06-21 10:11:11 -0700

    dbl-link-list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,25 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  const newNode = new Node(val, null, this.tail)
+  if (this.tail) this.tail.next = newNode;
+  else this.head = newNode;
+  this.tail = newNode;
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  let currNode = this.head;
+
+  while (currNode) {
+    if (currNode !== val) {
+      currNode = currNode.next;
+    }
+    delete currNode;
+  }
+  return LinkedList;
 };
 
 module.exports = LinkedList;
