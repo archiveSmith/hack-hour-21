ec8e37b37d4af87fdcb9d6622a1efdd377330021
Stefan <stefan.armijo@gmail.com>
2018-06-21 09:44:45 -0700

    implement doubly-linked-list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,43 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+
+    // grab tail of list
+    // point tail.next to new Node
+    // point new Node.prev to tail
+    // set this.tail to new Node
+    let currTail = this.tail;
+    let newTail = new Node(val);
+    
+    currTail.next = newTail;
+    newTail.prev = currTail;
+    this.tail = newTail;
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+
+    // iterate through list nodes
+    // if no node found return undefined or something
+    // if node found 
+        // set found.prev to found.next
+        // set found.next.prev to found.prev
+
+    let currNode = this.head;
+
+    while (currNode !== null) {
+        if (currNode.val === val) {
+            currNode.prev.next = currNode.next;
+            currNode.next.prev = currNode.prev;
+            return;
+        }
+
+        currNode = currNode.next;
+    }
+    return null;
 };
 
 module.exports = LinkedList;
+
