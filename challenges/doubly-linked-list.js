59e07aaa01d9f8fd8069827713127d9b006d9334
Jeff <jeffreyrkang@gmail.com>
2018-06-21 09:36:52 -0700

    Complete doubly-linked-list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,15 +16,49 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
+LinkedList.prototype.add = function (val) {
+  if (!this.head) this.head = this.tail = new Node(val);
+  else {
+    this.tail.next = new Node(val);
+    this.tail.next.prev = this.tail;
+    this.tail = this.tail.next;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
-  
+LinkedList.prototype.remove = function (val) {
+  let place = this.head;
+  while (place !== null && place.val !== val) {
+    place = place.next;
+  }
+  if (place === null) return undefined;
+  else if (place === this.head) {
+    place.next.prev = null;
+    this.head = place.next;
+  } else if (place === this.tail) {
+    place.prev.next = null;
+    this.tail = place.prev;
+  } else {
+    place.prev.next = place.next;
+    place.next.prev = place.prev;
+  }
 };
+
+// const linker = new LinkedList();
+// linker.add(1);
+// console.log(linker.tail);
+// linker.add(3);
+// console.log(linker.tail);
+// linker.add(10);
+// console.log(linker.tail);
+// linker.add(122);
+// console.log(linker.tail);
+// linker.remove(122);
+// console.log(linker.tail);
+// linker.remove(1);
+// console.log(linker.head);
+
 
 module.exports = LinkedList;
