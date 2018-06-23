d217578396903e8d4d18ed86e1c4e799155ab6b9
Will Howard <Will@Wills-MacBook-Pro.local>
2018-06-23 09:25:13 -0700

    bubble-sort soln

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -2,10 +2,20 @@
 
 // https://en.wikipedia.org/wiki/Bubble_sort
 
-
-
 function bubbleSort(array) {
-
+  let swapped = false;
+  for (let i = 1; i < array.length; i++) {
+    let temp;
+    if ( array[i] < array[i - 1] ) {
+      temp = array[i - 1];
+      array[i - 1] = array[i];
+      array[i] = temp;
+      swapped = true;
+    } 
+  }
+  if ( swapped !== false ) {
+    return bubbleSort(array);
+  } else return array;
 }
 
 module.exports = bubbleSort;
