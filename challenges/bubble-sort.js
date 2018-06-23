5cc3b38a1c8dcf3046c47bcf45a00392eb902176
James Tu <james@2-bit-toys.com>
2018-06-23 09:15:14 -0700

    bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,26 @@
 
 
 function bubbleSort(array) {
+  if (!array) return [];
+  if (array.length <= 1) return array;
+
+  let arr = [...array];
+
+  let temp;
+  for (let i = arr.length-2; i >= 0; i--) {
+    for (let j=0; j<=i; j++){
+      if (arr[j] > arr[j+1]){
+        temp = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  return arr;
 
 }
+
+// console.log(bubbleSort([2,3,1,100, -3]));
 
 module.exports = bubbleSort;
