24a981c4899713d9f3adb452b8b33d07d2a1755a
Colin McCarthy <colin@Colins-MacBook-Pro.local>
2018-06-23 09:13:05 -0700

    complete bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,19 @@
 
 
 function bubbleSort(array) {
-
+  let changed = true;
+  while (changed) {
+    changed = false;
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < array[i - 1]) {
+        const temp = array[i];
+        array[i] = array[i - 1];
+        array[i - 1] = temp;
+        changed = true;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
