f182d4f205117832c5e427b2aa81d81f40874f3e
Christopher <chris132128@gmail.com>
2018-06-23 09:37:18 -0700

    bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,19 @@
 
 
 function bubbleSort(array) {
-
+  let moved = false;
+  array.forEach((ele, index) => {
+    let temp = array[index + 1];
+    if (temp < ele) {
+      array[index + 1] = ele;
+      array[index] = temp;
+      moved = true;
+    }
+  });
+  if (moved) return bubbleSort(array);
+  else return array;
 }
+
+// console.log(bubbleSort([5, 4, 3, 1, 2, 8, 9, 15, 12]));
 
 module.exports = bubbleSort;
