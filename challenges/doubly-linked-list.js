9a48a421d514c08415f56dc77ec36a0bd5d47191
Tiffany Yang <teefyang@Tiffanys-MacBook-Pro.local>
2018-06-24 00:57:09 -0700

    resubmitted doubly-linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -18,7 +18,7 @@ Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
   //if the first Node, just create it
-  if(!head && !tail){
+  if(!this.head && !this.tail){
     let newNode = new Node(val);
     this.head = newNode;
     this.tail = newNode;
@@ -36,20 +36,38 @@ Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
   let newhead = this.head;
-  let newtail = this.tail;
   //moving from the head (forward), and moving from the back (backward)
-  while(this.newhead !== value){
-    if(this.newhead === val && this.newhead.prev === null) {
-      this.newhead.next.prev = null;
-      break;
-  }if (this.newhead === val){
-    this.newhead.prev.next = this.newhead.next;
-    thiis.newhead.next.prev = this.newhead.prev;
-  }else{
-    this.newhead = this.newhead.next;
+  while(newhead.val !== val && newhead.next !== null){
+    newhead = newhead.next;
+    // console.log('HERE IS THE NEWHEAD', newhead.val);
   }
-}//when meeting having, stop, and evaluate the index of val of the traversed position
-  
+    if(newhead.val === val && newhead.prev === null) {
+      newhead.next.prev = null;
+      this.head = newhead.next;
+    }else if (newhead.val === val && newhead.next === null){
+      // console.log("IN THE FINAL ELSE IF");
+      newhead.prev.next = null;
+      this.tail = newhead.prev;
+      //when meeting having, stop, and evaluate the index of val of the traversed position
+    }
+    else if (newhead.val === val){
+      // console.log('THIS IS IT');
+      newhead.prev.next = newhead.next;
+      newhead.next.prev = newhead.prev;
 };
+};
+// let node = new Node(5);
+// node.next = new Node(4);
+// node.next.prev = node;
+// node.next.next = new Node(3);
+// node.next.next.prev = node.next;
+
+// let link = new LinkedList();
+// link.head = node;
+// link.tail = node.next.next;
+
+// link.add(2);
+// link.remove(4);
+// console.log(link);
 
 module.exports = LinkedList;
