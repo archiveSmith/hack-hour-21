33b0e725066fca470071c2ecf901b1c1a6430a9a
William Adamowicz <william.adamowicz@gmail.com>
2018-06-25 09:46:26 -0700

    complete pb

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -26,12 +26,24 @@
 
 //  return the number associated with the name in the jazbook
 function findName(jazbook, name) {
-
+  let i = 0;
+  do {
+    if (jazbook[i][0] === name) return jazbook[i][1];
+  } while (i++ < jazbook.length - 1)
 }
 
 // return an object literal representing the jazbook
 function makePhoneBookObject(jazbook){
+  const pb = jazbook.reduce((pb, el) => {
+    pb[el[0]] = el[1];
+    return pb; 
+  }, {});
+  
+  pb.find = name => pb[name] ? pb[name] : '404'; 
+  pb.remove = name => delete pb[name];
+  pb.add = (name, num) => pb[name] ? 'name already in phonebook' : pb[name] = num;
 
+  return pb;
 }
 
 const objectToExport = {
@@ -40,3 +52,19 @@ const objectToExport = {
 };
 
 module.exports = objectToExport;
+
+let jazbook = [
+    ['alex','301-844-3421'],
+    ['jae','301-844-1211'],
+    ['david','301-844-0978'],
+    ['travis','301-844-8505'],
+    ['jasmine','1800-974-4539'],
+  ];
+
+// let pb = makePhoneBookObject(jazbook);
+// pb.add('wilbur', '123')
+// console.log(pb)
+// console.log(pb.find('wilbur'))
+// console.log(pb)
+// console.log(pb.remove('jasmine'))
+// console.log(pb)
