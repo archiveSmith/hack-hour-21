bfc1e36ae4711199308a47257482ec54438117dd
Colin McCarthy <colin@Colins-MacBook-Pro.local>
2018-06-25 09:33:11 -0700

    complete jazbook?

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -26,13 +26,44 @@
 
 //  return the number associated with the name in the jazbook
 function findName(jazbook, name) {
-
+  for (let i = 0; i < jazbook.length; i += 1) {
+    if (jazbook[i][0] === name) return jazbook[i][1];
+  }
+  return false;
 }
 
 // return an object literal representing the jazbook
-function makePhoneBookObject(jazbook){
-
+function makePhoneBookObject(jazbook) {
+  const jazbookObj = jazbook.reduce((acc, e) => {
+    acc[e[0]] = e[1];
+    return acc;
+  }, {});
+  jazbookObj.add = function add(name, number) {
+    this[name] = number;
+  };
+  jazbookObj.find = function find(name) {
+    return this[name];
+  };
+  jazbookObj.remove = function find(name) {
+    delete this[name];
+  };
+  return jazbookObj;
 }
+
+// const jazbook = [
+//   ['alex','301-844-3421'],
+//   ['jae','301-844-1211'],
+//   ['david','301-844-0978'],
+//   ['travis','301-844-8505'],
+//   ['jasmine','1800-974-4539'],
+// ];
+
+// console.log(findName(jazbook, 'jae'));
+// console.log(findName(jazbook, 'joe'));
+// const p = makePhoneBookObject(jazbook);
+// p.add('colin', '518-821-7481');
+// console.log(p.find('colin'));
+// p.remove('jae');
 
 const objectToExport = {
   findName,
