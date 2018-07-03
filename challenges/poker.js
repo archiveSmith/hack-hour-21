3f3547aae5421075e6fa1278bf90fd83b8e5f90e
Colin McCarthy <colin@Colins-MacBook-Pro.local>
2018-07-03 10:14:59 -0700

    complete poker

--- challenges/poker.js
+++ challenges/poker.js
@@ -20,7 +20,61 @@
  * BONUS2: Create a deck of cards function that generates two random hands for you.
  */
 function poker(hand1, hand2) {
-
+  if (hand1.length < 5 || hand2.length < 5) throw new Error('bad hand size!');
+  hand1 = hand1.sort();
+  hand2 = hand2.sort();
+  let hand1Value, hand2Value;
+  const fourOfAKind = hand => hand.reduce((acc, e) => {
+    acc[e] = acc[e] ? acc[e] + 1 : 1;
+    if (acc[e] === 4) acc.pass = true;
+    return acc;
+  }, { pass: false }).pass
+  const fullHouse = hand => hand.reduce((acc, e, i) => {
+    acc[e] = acc[e] ? acc[e] + 1 : 1;
+    if (i === 4 && Object.values(acc).includes(2) && Object.values(acc).includes(3)) acc.pass = true;
+    return acc;
+  }, { pass: false }).pass;
+  const straight = hand => hand.reduce((acc, e, i, arr) => {
+    if (acc && i !== 0 && arr[i - 1] !== e - 1) return false;
+    return acc;
+  }, true)
+  const threeOfAKind = hand => hand.reduce((acc, e) => {
+    acc[e] = acc[e] ? acc[e] + 1 : 1;
+    if (acc[e] === 3) acc.pass = true;
+    return acc;
+  }, { pass: false }).pass;
+  const twoPair = hand => hand.reduce((acc, e, i) => {
+    acc[e] = acc[e] ? acc[e] + 1 : 1;
+    if (i > 2) {
+      const check = Object.values(acc);
+      if (check.includes(2)) check.splice(check.indexOf(2), 1);
+      if (check.includes(2)) acc.pass = true;
+    }
+    return acc;
+  }, { pass: false }).pass;
+  const onePair = hand => hand.reduce((acc, e) => {
+    acc[e] = acc[e] ? acc[e] + 1 : 1;
+    if (acc[e] === 2) acc.pass = true;
+    return acc;
+  }, { pass: false }).pass;
+  const callbacks = [fourOfAKind, fullHouse, straight, threeOfAKind, twoPair, onePair];
+  for (let i = 0; i < callbacks.length; i++) {
+    hand1Value = callbacks[i](hand1);
+    hand2Value = callbacks[i](hand2);
+    if (hand1Value > hand2Value) return "Player 1 Wins";
+    if (hand2Value > hand1Value) return "Player 2 Wins";
+    if (hand1Value && hand2Value) {
+      hand1Value = Math.max(...hand1);
+      hand2Value = Math.max(...hand2);
+      if (hand1Value > hand2Value) return "Player 1 Wins";
+      if (hand2Value > hand1Value) return "Player 2 Wins";
+    }
+  }
+  hand1Value = Math.max(...hand1);
+  hand2Value = Math.max(...hand2);
+  if (hand1Value > hand2Value) return "Player 1 Wins";
+  if (hand2Value > hand1Value) return "Player 2 Wins";
+  return "Draw";
 }
 
 module.exports = poker;
