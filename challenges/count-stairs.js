51db7c2b1315961bd23d5a76a0b88c929a8d23bb
Jeff <jeffreyrkang@gmail.com>
2018-07-03 10:41:03 -0700

    Complete count-stairs and poker

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -15,7 +15,11 @@
  */
 
 function countStairs(n) {
-
+  if (n === 0) return 1;
+  if (n < 0) return 0;
+  return countStairs(n - 1) + countStairs(n - 2);
 }
+
+// console.log(countStairs(5));
 
 module.exports = countStairs;
--- challenges/poker.js
+++ challenges/poker.js
@@ -19,8 +19,45 @@
  * BONUS: Account for suits and add in Flush & Straight Flush/Royal Flush.
  * BONUS2: Create a deck of cards function that generates two random hands for you.
  */
-function poker(hand1, hand2) {
 
+function maxScore(hand) {
+  let score = 0;
+  let count = {};
+  hand.forEach((element) => {
+    if (!count[element]) count[element] = 1;
+    else count[element] += 1;
+  });
+  let pairs = 0;
+  Object.keys(count).forEach((element) => {
+    if (count[element] === 4) score = 6;
+    else if (count[element] === 3 && pairs === 1 && score < 5) score = 5;
+    else if (count[element] === 3 && pairs === 0 && score < 3) score = 3;
+    else if (count[element] === 2 && score === 3 && score < 5) score = 5;
+    else if (count[element] === 2 && pairs === 1 && score < 2) score = 2;
+    else if (count[element] === 2 && pairs === 0 && score === 0) score = 1;
+  });
+  for (let i = 0; i < hand.length - 1; i += 1) {
+    hand[i] = hand[i + 1] - hand[i];
+  }
+  hand.pop();
+  if (hand.every(a => a === 1)) return 4;
+  return score;
 }
+
+function poker(hand1, hand2) {
+  hand1 = hand1.sort();
+  hand2 = hand2.sort();
+  const score1 = maxScore(hand1);
+  const score2 = maxScore(hand2);
+  if (score1 === 0 && score2 === 0) {
+    if (hand1[4] === hand2[4]) return 'Draw';
+    else if (hand1[4] > hand2[4]) return 'Player 1 wins';
+    return 'Player 2 wins';
+  } else if (score1 === score2) return 'Draw';
+  else if (score1 > score2) return 'Player 1 wins';
+  return 'Player 2 wins';
+}
+
+// console.log(poker([3,5,5,5,2],[4,6,7,8,8]));
 
 module.exports = poker;
