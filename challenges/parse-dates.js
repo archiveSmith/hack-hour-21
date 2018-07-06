af7247c1ea831aa12d77ed1be55f47f62cc635b8
Jeff <jeffreyrkang@gmail.com>
2018-07-06 10:36:15 -0700

    Complete parse-dates

--- challenges/parse-dates.js
+++ challenges/parse-dates.js
@@ -40,7 +40,58 @@
 // - if any part of the date string is missing then you can consider it an invalid date
 
 function parseDates(str) {
-  
+  const parts = str.split(' ');
+  if (parts.length < 3) return Date.now();
+  if (parts[0].length === 3) { // Month first
+    const monthIndex = {
+      Jan: 0,
+      Feb: 1,
+      Mar: 2,
+      Apr: 3,
+      May: 4,
+      Jun: 5,
+      Jul: 6,
+      Aug: 7,
+      Sep: 8,
+      Oct: 9,
+      Nov: 10,
+      Dec: 11,
+    };
+    if (parts[0] !== 'Jan' && !monthIndex[parts[0]]) return Date.now();
+    const month = monthIndex[parts[0]];
+    if (parts[1].length < 3) return Date.now();
+    const day = +parts[1].slice(0, parts[1].length - 2);
+    const time = parts[2].split(':');
+    let hours = +time[0];
+    const minutes = +time[1];
+    if (hours !== 12 && parts[3] === 'PM') hours += 12;
+    if (hours === 12 && parts[3] === 'AM') hours -= 12;
+    return new Date(2018, month, day, hours - 8, minutes);
+  }
+  const dayIndex = {
+    Today: 6,
+    Monday: 2,
+    Tuesday: 3,
+    Wednesday: 4,
+    Thursday: 5,
+    Friday: 29,
+    Saturday: 30,
+    Sunday: 1,
+  };
+  if (!dayIndex[parts[0]]) return Date.now();
+  const day = dayIndex[parts[0]];
+  const month = parts[0] === 'Friday' || parts[0] === 'Saturday' ? 5 : 6;
+  const time = parts[1].split(':');
+  let hours = +time[0];
+  const minutes = +time[1];
+  if (hours !== 12 && parts[2] === 'PM') hours += 12;
+  if (hours === 12 && parts[2] === 'AM') hours -= 12;
+  return new Date(2018, month, day, hours - 7, minutes);
 }
+
+// console.log(parseDates('Jan 12th 1:09 AM'));
+// console.log(parseDates('Jan 12th 8:19 PM'));
+// console.log(parseDates('Today 8:15 PM'));
+// console.log(parseDates('Sunday 12:59 PM'));
 
 module.exports = parseDates;
