4d23a554f29064d7295daadb3ab20cc101bc9bf2
Colin McCarthy <colin@Colins-MacBook-Pro.local>
2018-07-07 10:25:30 -0700

    complete common elements

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -12,8 +12,35 @@
 
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
-function commonElements(array1, array2, array3, array4){
+// BRUTE FORCE -- O(n^2)
+// function commonElements(array1, array2, array3, array4) {
+//   return array1.reduce((acc, e, i) => {
+//     if (array2.includes(e) && array3.includes(e) && array4.includes(e) && !acc.includes(e)) acc.push(e);
+//     if (i === array1.length - 1 && acc.length === 0) acc = 'Nothing in Common!';
+//     return acc;
+//   }, []);
+// }
 
+// O(2n)
+function commonElements(array1, array2, array3, array4) {
+  const cache = {};
+  for (let a = 0; a < 4; a++) {
+    const array = arguments[a];
+    for (let i = 0; i < array.length; i++) {
+      const e = array[i];
+      const iStr = (a + 1).toString(10);
+      if (!cache[e]) cache[e] = iStr;
+      else if (cache[e][cache[e].length - 1] !== iStr) cache[e] += iStr;
+    }
+  }
+  const result = [];
+  for (let key in cache) {
+    if (cache[key] === '1234') result.push(key);
+  }
+  return result.length ? result : 'Nothing in Common!';
 }
+
+// console.log(commonElements(array1, array2, array3, array4));
+
 
 module.exports = commonElements;
