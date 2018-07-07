db04a74956c166d96d87a0b02f0af06c34ba407d
Christopher <chris132128@gmail.com>
2018-07-07 09:41:52 -0700

    common elements

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -3,17 +3,23 @@
 // duplicates are only counted once;
 // for example == given the following input
 
-  // var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
-  // var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
-  // var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
-  // var array4 = ['ferret',12,12,45,9,66,77,78,2000];
+  var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
+  var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
+  var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
+  var array4 = ['ferret',12,12,45,9,66,77,78,2000];
 
 // your output would be [ 12, 'ferret']
 
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
 function commonElements(array1, array2, array3, array4){
-
+  return array1.reduce((acc, next) => {
+    console.log(acc);
+    if (array2.indexOf(next) !== -1 && array3.indexOf(next) !== -1 && array4.indexOf(next) !== -1 && acc.indexOf(next) === -1) acc.push(next);
+    return acc;
+  }, []);
 }
+
+console.log(commonElements(array1, array2, array3, array4));
 
 module.exports = commonElements;
