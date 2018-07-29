f32a2b1b5e5435f37488da3ee770209d8e0abccb
VernSul <vicfaucon@gmail.com>
2018-07-28 22:46:57 -0700

    HH

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -18,17 +18,26 @@ function commonElements(array1, array2, array3, array4){
     if(!output.includes(element)) output.push(element)    
   });
 
-  if(!arguments[1]){
+  
+  if(!array2){
+    output = output.length ? output : 'Nothing in Common!';
     return output;
   } else {
-
     for(let i = 0; i < output.length; i++){
-      if(!array2.includes(output[i])) output.splice(i, 1);    
+      if(!array2.includes(output[i])) {
+        output.splice(i, 1);
+        i = i-1;
+      }    
     }
     return commonElements(output, arguments[2], arguments[3]);
     }
 
 }
 
+  var array1 = [1,4,6,7,99,2000,'dog','dog',99,1000];
+  var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
+  var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
+  var array4 = ['ferret',12,12,45,9,66,77,78,2000];
+  console.log(commonElements(array1, array2, array3, array4));
 
 module.exports = commonElements;
